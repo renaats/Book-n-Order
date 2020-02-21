ff8888c4079590f2158fafd22489922a7c05f3aa
jdeweerdt <j.c.h.p.deweerdt@student.tudelft.nl>
2020-02-21 21:44:12 +0100

    Reversed previous commit (removal of demo classes to pass pipelines)

---
+++ server/src/main/java/nl/tudelft/oopp/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package nl.tudelft.oopp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages={"nl.tudelft.oopp.demo.repositories", "nl.tudelft.oopp.demo.controllers"})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
