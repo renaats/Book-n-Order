aace1e48298e1fcbe0d625dfbb8d5bbb16609691
rjursevskis <r.jursevskis@student.tudelft.nl>
2020-02-22 18:07:17 +0100

    Added client side functionality to add a new room and list all current rooms form the database

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
@@ -6,13 +6,35 @@ import nl.tudelft.oopp.demo.communication.ServerCommunication;
 public class MainSceneController {
 
     /**
-     * Handles clicking the button.
+     * Handles clicking the quote button.
      */
-    public void buttonClicked() {
+    public void quoteButtonClicked() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Quote for you");
         alert.setHeaderText(null);
         alert.setContentText(ServerCommunication.getQuote());
+        alert.showAndWait();
+    }
+
+    /**
+     * Handles clicking the add button.
+     */
+    public void addBuildingButtonClicked() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("A new building has been added!");
+        alert.setHeaderText(null);
+        alert.setContentText(ServerCommunication.postBuilding());
+        alert.showAndWait();
+    }
+
+    /**
+     * Handles clicking the list button.
+     */
+    public void listBuildingsButtonClicked() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("All buildings:");
+        alert.setHeaderText(null);
+        alert.setContentText(ServerCommunication.getBuilding());
         alert.showAndWait();
     }
 }
