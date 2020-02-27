d67ab3e8b18d379cb88598b27676f7e895e035ef
ljongejans <l.j.jongejans@student.tudelft.nl>
2020-02-27 19:31:07 +0100

    basic layout for view page

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/TemplateController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/TemplateController.java
@@ -1,4 +1,11 @@
 package nl.tudelft.oopp.demo.controllers;
 
 public class TemplateController {
+
+    /**
+     * Template for all pages
+     */
+    public void template(){
+
+    }
 }
--- client/src/main/java/nl/tudelft/oopp/demo/views/TemplateDisplay.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/TemplateDisplay.java
@@ -1,4 +1,35 @@
 package nl.tudelft.oopp.demo.views;
 
-public class TemplateDisplay {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class TemplateDisplay extends Application implements EventHandler<ActionEvent> {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/template.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+
+    }
 }
