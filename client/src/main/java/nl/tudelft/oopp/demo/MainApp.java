c4802e9e4f28ff6706659f59dc2f35716fe92207
ljongejans <l.j.jongejans@student.tudelft.nl>
2020-02-28 16:48:30 +0100

    my profile page view

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -1,9 +1,9 @@
 package nl.tudelft.oopp.demo;
 
-import nl.tudelft.oopp.demo.views.QuoteDisplay;
+import nl.tudelft.oopp.demo.views.MyAccountDisplay;
 
 public class MainApp {
     public static void main(String[] args) {
-        QuoteDisplay.main(new String[0]);
+        MyAccountDisplay.main(new String[0]);
     }
 }
