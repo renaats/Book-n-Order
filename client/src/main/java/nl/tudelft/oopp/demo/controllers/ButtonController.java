fbd182ccfab1e203270260b7ac56e74f5d5359ef
jdeweerdt <J.C.H.P.deWeerdt@student.tudelft.nl>
2020-02-28 17:05:05 +0100

    Login page and GUI working properly

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/ButtonController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/ButtonController.java
@@ -1,9 +1,11 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import java.io.IOException;
 import javafx.scene.control.Alert;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
+import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
-public class MainSceneController {
+public class ButtonController {
 
     /**
      * Handles clicking the quote button.
@@ -48,6 +50,18 @@ public class MainSceneController {
         alert.setContentText(ServerCommunication.getBuilding());
         alert.showAndWait();
     }
+
+    /**
+     * Changes to mainScene.fxml.
+     * @throws IOException again, all input will be valid. No need to check this, thus we throw.
+     */
+    public void mainMenu() throws IOException {
+        ApplicationDisplay.changeScene("/mainScene.fxml");
+    }
+
+    //    public void registerScene() throws IOException {
+    //        ApplicationDisplay.changeScene();
+    //    }
 
     /**
      * Handles clicking the remove button.
--- client/src/main/java/nl/tudelft/oopp/demo/views/ApplicationDisplay.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/ApplicationDisplay.java
@@ -9,10 +9,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class QuoteDisplay extends Application {
+public class ApplicationDisplay extends Application {
+
+    private static Stage primaryStage;
+
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
         FXMLLoader loader = new FXMLLoader();
         URL xmlUrl = getClass().getResource("/login-screen.fxml");
         loader.setLocation(xmlUrl);
@@ -20,6 +24,18 @@ public class QuoteDisplay extends Application {
 
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    /**
+     * Changes the scene to whatever file you input.
+     * @param fxml = file string of scene you want to change to.
+     * @throws IOException = All input will be valid, no need to check, this we throw.
+     */
+    public static void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(
+                ApplicationDisplay.class.getResource(fxml));
+
+        primaryStage.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) {
