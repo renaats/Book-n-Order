34af9fd250736ab976cb684708d3cb3dd51f92b1
ljongejans <l.j.jongejans@student.tudelft.nl>
2020-02-28 11:03:01 +0100

    templatecontroller update

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/TemplateController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/TemplateController.java
@@ -1,11 +1,32 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.scene.control.Alert;
+import nl.tudelft.oopp.demo.views.QuoteDisplay;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import nl.tudelft.oopp.demo.communication.ServerCommunication;
+import nl.tudelft.oopp.demo.views.TemplateDisplay;
+import nl.tudelft.oopp.demo.views.QuoteDisplay;
+
+import java.io.IOException;
+import java.net.URL;
+
+
 public class TemplateController {
 
     /**
      * Template for all pages
      */
     public void template(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("not implememnted");
+        alert.setHeaderText(null);
+        alert.showAndWait();
+    }
 
+    public void roomsSelected() throws IOException {
+        QuoteDisplay.main(new String[0]);
     }
 }
