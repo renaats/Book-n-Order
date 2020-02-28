590d7f8aa716a95974e5c1a8234d4ac21f3f2395
Alex De Los Santos Subirats <A.DeLosSantosSubirats@student.tudelft.nl>
2020-02-28 16:09:05 +0100

    Added back buttons to all windows and duplicated the (book room button))
    the book rooms button should lead to the reservation page and the "To main scene" should lead to the current main scene

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeReservationController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeReservationController.java
@@ -2,11 +2,18 @@ package nl.tudelft.oopp.demo.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -44,5 +51,14 @@ final ObservableList list= FXCollections.observableArrayList();
         list.addAll(a,b,c);
         Pick.getItems().addAll(list);
         Drop.getItems().addAll(list);
+    }
+
+    public void goBackToMenu(ActionEvent actionEvent) throws IOException {
+        Parent roomSelectParent = FXMLLoader.load(getClass().getResource("/mainMenu.fxml"));
+        Scene roomSelectScene = new Scene (roomSelectParent);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(roomSelectScene);
+        window.show();
     }
 }
