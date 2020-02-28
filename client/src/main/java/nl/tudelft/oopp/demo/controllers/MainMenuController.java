099fa8eca4461be9fce4ee29ec336a361bb0b54d
Alto Delia <A.delia@student.tudelft.nl>
2020-02-28 16:07:33 +0100

    Added Calendar and Drop Down Menu to Room Book view (with fixed checkstyle issues)

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainMenuController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainMenuController.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.demo.controllers;
-
+import java.io.IOException;
+import java.net.URL;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -7,19 +8,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import javax.swing.*;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.views.MainMenuDisplay;
 import nl.tudelft.oopp.demo.views.QuoteDisplay;
 
-import javax.swing.*;
-import java.io.IOException;
-import java.net.URL;
 
 public class MainMenuController {
 
     /**
      * Handles clicking the food button.
      */
+    @SuppressWarnings({"checkstyle:MethodParamPad", "CheckStyle"})
     public void foodsSelsected(ActionEvent event) throws IOException {
         Parent roomSelectParent = FXMLLoader.load(getClass().getResource("/mainScene.fxml"));
         Scene roomSelectScene = new Scene (roomSelectParent);
@@ -32,6 +32,7 @@ public class MainMenuController {
     /**
      * Handles clicking the bikes button.
      */
+    @SuppressWarnings({"checkstyle:MethodParamPad", "CheckStyle"})
     public void bikesSelected(ActionEvent event) throws IOException {
         Parent roomSelectParent = FXMLLoader.load(getClass().getResource("/mainScene.fxml"));
         Scene roomSelectScene = new Scene (roomSelectParent);
@@ -43,8 +44,10 @@ public class MainMenuController {
     /**
      * Handles clicking the rooms button. Should send you to the mainScene.
      */
+
+    @SuppressWarnings({"checkstyle:MethodParamPad", "checkstyle:EmptyLineSeparator", "CheckStyle"})
     public void roomsSelected(ActionEvent event) throws IOException {
-        Parent roomSelectParent = FXMLLoader.load(getClass().getResource("/mainScene.fxml"));
+        Parent roomSelectParent = FXMLLoader.load(getClass().getResource("/bookRoom.fxml"));
         Scene roomSelectScene = new Scene (roomSelectParent);
 
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
