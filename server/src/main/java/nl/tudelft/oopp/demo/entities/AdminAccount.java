20110276d7dd06fad9e335015d89a9f94849ab65
rjursevskis <r.jursevskis@student.tudelft.nl>
2020-02-28 09:55:08 +0100

    Refactored the User class to have a list of Roles instead of using inheritance

--- server/src/main/java/nl/tudelft/oopp/demo/entities/AdminAccount.java
+++
@@ -1,8 +0,0 @@
-package nl.tudelft.oopp.demo.entities;
-
-import javax.persistence.Entity;
-
-@Entity
-public abstract class AdminAccount extends Account {
-
-}
--- server/src/main/java/nl/tudelft/oopp/demo/repositories/RoleRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/RoleRepository.java
@@ -1,10 +1,10 @@
 package nl.tudelft.oopp.demo.repositories;
 
-import nl.tudelft.oopp.demo.entities.Account;
+import nl.tudelft.oopp.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, String> {
+public interface RoleRepository extends JpaRepository<User, String> {
 
 }
