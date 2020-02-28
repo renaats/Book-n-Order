c44216c795e68cb680b891f7c0bc0d5f3b8c739e
Alex De Los Santos Subirats <A.DeLosSantosSubirats@student.tudelft.nl>
2020-02-28 15:27:32 +0100

    Added basic UI for bike Reservation and basic functionality

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeReservationController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeReservationController.java
@@ -1,4 +1,48 @@
 package nl.tudelft.oopp.demo.controllers;
 
-public class BikeReservationController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BikeReservationController implements Initializable {
+final ObservableList list= FXCollections.observableArrayList();
+    @FXML
+    private ChoiceBox<String> Pick;
+    @FXML
+    private  ChoiceBox<String> Drop;
+    @FXML
+    private TextField screen;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadData();
+    }
+
+    @FXML
+    private void reserveBike (){
+        String bike = Pick.getValue()+ Drop.getValue();
+        //noinspection ConstantConditions
+        if (bike == null){
+            screen.setText("No bike");
+        }
+        else{
+            screen.setText("your bike is "+bike);
+        }
+    }
+
+    public  void loadData(){
+        list.removeAll(list);
+        String a= "1";
+        String b = "2";
+        String c = "3";
+        list.addAll(a,b,c);
+        Pick.getItems().addAll(list);
+        Drop.getItems().addAll(list);
+    }
 }
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainMenuController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainMenuController.java
@@ -5,22 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
-import nl.tudelft.oopp.demo.communication.ServerCommunication;
-import nl.tudelft.oopp.demo.views.MainMenuDisplay;
-import nl.tudelft.oopp.demo.views.QuoteDisplay;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.net.URL;
 
 public class MainMenuController {
 
     /**
      * Handles clicking the food button.
      */
-    public void foodsSelsected(ActionEvent event) throws IOException {
+    public void foodsSelected(ActionEvent event) throws IOException {
         Parent roomSelectParent = FXMLLoader.load(getClass().getResource("/mainScene.fxml"));
         Scene roomSelectScene = new Scene (roomSelectParent);
 
@@ -33,7 +27,7 @@ public class MainMenuController {
      * Handles clicking the bikes button.
      */
     public void bikesSelected(ActionEvent event) throws IOException {
-        Parent roomSelectParent = FXMLLoader.load(getClass().getResource("/mainScene.fxml"));
+        Parent roomSelectParent = FXMLLoader.load(getClass().getResource("/Bike reservations.fxml"));
         Scene roomSelectScene = new Scene (roomSelectParent);
 
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
