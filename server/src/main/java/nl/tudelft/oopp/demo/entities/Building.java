3b41b3842b8074400473ba1570aec4024f0b2e9d
rjursevskis <r.jursevskis@student.tudelft.nl>
2020-02-29 21:34:41 +0100

    Added a room class and all of the necessary connections to the building class. Changed some paths to make mroe sense. Breaks the GUI!

--- server/src/main/java/nl/tudelft/oopp/demo/entities/Building.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Building.java
@@ -1,9 +1,12 @@
 package nl.tudelft.oopp.demo.entities;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class Building {
@@ -12,7 +15,11 @@ public class Building {
     private Integer id;
 
     private String name;
-    private String email;
+    private String street;
+    private int houseNumber;
+
+    @OneToMany(mappedBy = "building")
+    Set<Room> rooms = new HashSet<>();
 
     public void setId(Integer id) {
         this.id = id;
@@ -22,9 +29,18 @@ public class Building {
         this.name = name;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setStreet(String street) {
+        this.street = street;
     }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public void setRooms(Set<Room> rooms) {
+        this.rooms = rooms;
+    }
+
 
     public Integer getId() {
         return id;
@@ -34,7 +50,35 @@ public class Building {
         return name;
     }
 
-    public String getEmail() {
-        return email;
+    public String getStreet() {
+        return street;
     }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public Set<Room> getRooms() {
+        return rooms;
+    }
+
+
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
+
+    /**
+     * Checks if this building has a room with the specified name.
+     * @param name = the name of the room
+     * @return a boolean whether this building has the specified room
+     */
+    public boolean hasRoomWithName(String name) {
+        for (Room room: rooms) {
+            if (room.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
---
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Room.java
@@ -0,0 +1,104 @@
+package nl.tudelft.oopp.demo.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn
+    private Building building;
+
+    private String faculty;
+    private boolean facultySpecific;
+    private boolean projector;
+    private boolean screen;
+    private int nrPeople;
+    private int plugs;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public void setFacultySpecific(boolean facultySpecific) {
+        this.facultySpecific = facultySpecific;
+    }
+
+    public void setProjector(boolean projector) {
+        this.projector = projector;
+    }
+
+    public void setScreen(boolean screen) {
+        this.screen = screen;
+    }
+
+    public void setNrPeople(int nrPeople) {
+        this.nrPeople = nrPeople;
+    }
+
+    public void setPlugs(int plugs) {
+        this.plugs = plugs;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public boolean isFacultySpecific() {
+        return facultySpecific;
+    }
+
+    public boolean hasProjector() {
+        return projector;
+    }
+
+    public boolean hasScreen() {
+        return screen;
+    }
+
+    public int getNrPeople() {
+        return nrPeople;
+    }
+
+    public int getPlugs() {
+        return plugs;
+    }
+}
