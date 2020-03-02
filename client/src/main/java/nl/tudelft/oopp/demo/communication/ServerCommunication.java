b35686ce61298ffeb834d2d615ee780a1b51820a
Alex de Los Santos Subirats <a.delossantossubirats@student.tudelft.nl>
2020-03-02 22:35:13 +0100

    Update ServerCommunication.java

--- client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
@@ -5,7 +5,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-@SuppressWarnings("JavaDoc")
 public class ServerCommunication {
 
     private static final HttpClient client = HttpClient.newBuilder().build();
