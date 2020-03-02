f442a15f47d74487d67841779805c4b3a6337054
rjursevskis <r.jursevskis@student.tudelft.nl>
2020-03-02 13:03:32 +0100

    Added a test for the room reservation class. There are problems with foreign key consistency that should be fixed later

---
+++ server/src/test/java/nl/tudelft/oopp/demo/RoomReservationTest.java
@@ -0,0 +1,139 @@
+package nl.tudelft.oopp.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+import java.util.Date;
+import java.util.HashSet;
+
+import nl.tudelft.oopp.demo.entities.Building;
+import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.entities.RoomReservation;
+import nl.tudelft.oopp.demo.entities.User;
+import nl.tudelft.oopp.demo.repositories.BuildingRepository;
+import nl.tudelft.oopp.demo.repositories.RoomRepository;
+import nl.tudelft.oopp.demo.repositories.RoomReservationRepository;
+import nl.tudelft.oopp.demo.repositories.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class RoomReservationTest {
+    @Autowired
+    private RoomReservationRepository roomReservationRepository;
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BuildingRepository buildingRepository;
+
+    RoomReservation roomReservation;
+    RoomReservation roomReservation2;
+    Room room;
+    User user;
+    Building building;
+
+    /** Sets up the classes before executing the tests.
+     */
+    @BeforeEach
+    public void setup() {
+        building = new Building();
+        building.setName("EWI");
+        building.setStreet("Mekelweg");
+        building.setHouseNumber(4);
+        buildingRepository.saveAndFlush(building);
+
+        room = new Room();
+        room.setName("Ampere");
+        room.setBuilding(building);
+        room.setFaculty("EWI");
+        room.setFacultySpecific(false);
+        room.setScreen(true);
+        room.setProjector(true);
+        room.setNrPeople(300);
+        room.setPlugs(250);
+        room.setRoomReservations(new HashSet<>());
+        roomRepository.saveAndFlush(room);
+
+        user = new User();
+        user.setEmail("R.Jursevskis@student.tudelft.nl");
+        user.setPassword("1234");
+        user.setName("Renats");
+        user.setSurname("Jursevskis");
+        user.setFaculty("EWI");
+        user.setRoomReservations(new HashSet<>());
+        userRepository.saveAndFlush(user);
+
+        roomReservation = new RoomReservation();
+        roomReservation.setUser(userRepository.findAll().get(0));
+        roomReservation.setRoom(roomRepository.findAll().get(0));
+        roomReservation.setFromTime(new Date(10000000000L));
+        roomReservation.setToTime(new Date(11000000000L));
+        roomReservationRepository.saveAndFlush(roomReservation);
+        roomReservation = roomReservationRepository.findAll().get(0);
+    }
+
+    @Test
+    public void saveAndRetrieveRoomReservation() {
+        roomReservation2 = roomReservationRepository.findAll().get(0);
+        assertEquals(roomReservation, roomReservation2);
+    }
+
+    @Test
+    public void testGetters() {
+        roomReservation2 = roomReservationRepository.findAll().get(0);
+        assertEquals(roomReservation.getUser(), roomReservation2.getUser());
+        assertEquals(roomReservation.getRoom(), roomReservation2.getRoom());
+        assertEquals(roomReservation.getFromTime(), roomReservation2.getFromTime());
+        assertEquals(roomReservation.getToTime(), roomReservation2.getToTime());
+    }
+
+    @Test
+    public void testEqualRoomReservations() {
+        roomReservation2 = new RoomReservation();
+        roomReservation2.setUser(user);
+        roomReservation2.setRoom(room);
+        roomReservation2.setFromTime(new Date(10000000000L));
+        roomReservation2.setToTime(new Date(11000000000L));
+        assertEquals(roomReservation, roomReservation2);
+        assertNotSame(roomReservation, roomReservation2);
+    }
+
+    //    @Test
+    //    public void testOverlappingReservation() {
+    ////        room = roomRepository.findAll().get(0);
+    ////        System.out.println(roomRepository.findAll().size());
+    ////        System.out.println(room.getId());
+    ////        System.out.println(room.getName());
+    ////        System.out.println(room);
+    ////        System.out.println(room.getRoomReservations());
+    ////        System.out.println(roomReservation.getRoom());
+    //
+    //        assertTrue(room.hasRoomReservations());
+    //        assertTrue(room.getRoomReservations().contains(roomReservation));
+    //        assertTrue(room.hasRoomReservationBetween(new Date(10500000000L), new Date(12000000000L)));
+    //        assertTrue(room.hasRoomReservationBetween(new Date(9900000000L), new Date(10500000000L)));
+    //        assertTrue(room.hasRoomReservationBetween(new Date(10000000000L), new Date(1100000000L)));
+    //        assertTrue(room.hasRoomReservationBetween(new Date(10500000000L), new Date(1060000000L)));
+    //        assertFalse(room.hasRoomReservationBetween(new Date(11500000000L), new Date(1160000000L)));
+    //    }
+
+
+    /** Deletes everything from the repositories after testing.
+     */
+    @AfterEach
+    public void cleanup() {
+        roomReservationRepository.deleteAll();
+        roomRepository.deleteAll();
+        userRepository.deleteAll();
+        buildingRepository.deleteAll();
+    }
+}
