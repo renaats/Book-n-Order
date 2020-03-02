5f0a9d42b05c09afa0ba9e76218d4817518e919a
jdeweerdt <J.C.H.P.deWeerdt@student.tudelft.nl>
2020-03-02 15:07:13 +0100

    Fixed an import

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomController.java
@@ -8,6 +8,8 @@ import nl.tudelft.oopp.demo.repositories.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
