e43d52f06ed9e2fd02ae7433f4287e6b756497e8
rjursevskis <r.jursevskis@student.tudelft.nl>
2020-03-02 07:49:26 +0100

    Added a test for the class building

---
+++ server/src/test/java/nl/tudelft/oopp/demo/BuildingTest.java
@@ -0,0 +1,88 @@
+package nl.tudelft.oopp.demo;
+
+import nl.tudelft.oopp.demo.entities.Building;
+import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.repositories.BuildingRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class BuildingTest {
+    @Autowired
+    private BuildingRepository buildingRepository;
+
+    Building building;
+    Building building2;
+    Room room;
+    Set<Room> rooms;
+
+    @BeforeEach
+    public void setup() {
+        building = new Building();
+        building.setName("EWI");
+        building.setStreet("Mekelweg");
+        building.setHouseNumber(4);
+        buildingRepository.save(building);
+
+        room = new Room();
+        room.setName("Ampere");
+        room.setBuilding(building);
+        room.setFaculty("EWI");
+        room.setFacultySpecific(false);
+        room.setScreen(true);
+        room.setProjector(true);
+        room.setNrPeople(300);
+        room.setPlugs(250);
+
+        rooms = new HashSet<>();
+        rooms.add(room);
+    }
+
+    @Test
+    public void saveAndRetrieveBuilding() {
+        building2 = buildingRepository.getOne(1);
+        assertSame(building, building2);
+    }
+
+    @Test
+    public void testGetters() {
+        building2 = buildingRepository.findAll().get(0);
+        assertEquals("EWI", building2.getName());
+        assertEquals("Mekelweg", building2.getStreet());
+        assertEquals(4, building2.getHouseNumber());
+        assertEquals(building.getId(), building2.getId());
+        assertFalse(building.hasRooms());
+    }
+
+    @Test
+    public void testRooms() {
+        building.setRooms(rooms);
+        assertEquals(rooms, building.getRooms());
+        assertTrue(building.hasRooms());
+        assertTrue(building.hasRoomWithName("Ampere"));
+        assertFalse(building.hasRoomWithName("Boole"));
+    }
+
+    @Test
+    public void testEqualBuildings() {
+        building2 = new Building();
+        building2.setName("EWI");
+        building2.setStreet("Mekelweg");
+        building2.setHouseNumber(4);
+        assertEquals(building, building2);
+        assertNotSame(building, building2);
+    }
+
+    @AfterEach
+    public void cleanup() {
+        buildingRepository.deleteAll();
+    }
+}
