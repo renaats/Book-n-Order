75b38540b6c44b0f1cf2f86bc2982e3478fd09a4
Alto Delia <a.delia@student.tudelft.nl>
2020-03-05 15:31:08 +0100

    Added line separation of imports

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainMenuController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainMenuController.java
@@ -1,12 +1,14 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
 public class MainMenuController {
