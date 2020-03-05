07c7b06a511dcb379dcc1633dfcec9370bc7d1ec
Alto Delia <a.delia@student.tudelft.nl>
2020-03-05 15:06:08 +0100

    Fixed naming scheme in "loadRoomData" method.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BookRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BookRoomController.java
@@ -11,8 +11,8 @@ import javafx.scene.control.TextField;
 
 public class BookRoomController implements Initializable {
 
-    final ObservableList list = FXCollections.observableArrayList();
-    final ObservableList lisT = FXCollections.observableArrayList();
+    final ObservableList listOfRooms = FXCollections.observableArrayList();
+    final ObservableList listOfTimeSlots = FXCollections.observableArrayList();
     final ObservableList listOfBuildings = FXCollections.observableArrayList();
 
     @FXML
@@ -44,15 +44,15 @@ public class BookRoomController implements Initializable {
      * Adds the items to the choice boxes
      */
     public void loadRoomData() {
-        list.removeAll(list);
-        lisT.removeAll(lisT);
+        listOfRooms.removeAll(listOfRooms);
+        listOfTimeSlots.removeAll(listOfTimeSlots);
         listOfBuildings.removeAll(listOfBuildings);
         String none = "-";
         String a = "1";
         String b = "2";
         String c = "3";
-        list.addAll(none,a,b,c);
-        roomDropDown.getItems().addAll(list);
+        listOfRooms.addAll(none,a,b,c);
+        roomDropDown.getItems().addAll(listOfRooms);
 
         String building1 = "building1";
         String building2 = "building2";
@@ -60,25 +60,25 @@ public class BookRoomController implements Initializable {
         listOfBuildings.addAll(none,building1,building2,building3);
         buildingList.getItems().addAll(listOfBuildings);
 
-        lisT.add(none);
+        listOfTimeSlots.add(none);
 
         for (int i = 0; i < 24; i++) {
             for (int u = 0; u <= 45; u = u + 15) {
                 if (i == 0 && u == u) {
-                    if (!lisT.contains("00:00")) {
-                        lisT.add("00:00");
+                    if (!listOfTimeSlots.contains("00:00")) {
+                        listOfTimeSlots.add("00:00");
                     }
                 } else if (u == 0) {
-                    lisT.add(i + ":00");
+                    listOfTimeSlots.add(i + ":00");
                 } else if (i == 0) {
-                    lisT.add("00:" + u);
+                    listOfTimeSlots.add("00:" + u);
                 } else {
-                    lisT.add(i + ":" + u);
+                    listOfTimeSlots.add(i + ":" + u);
                 }
             }
         }
-        from.getItems().addAll(lisT);
-        until.getItems().addAll(lisT);
+        from.getItems().addAll(listOfTimeSlots);
+        until.getItems().addAll(listOfTimeSlots);
 
     }
 }
