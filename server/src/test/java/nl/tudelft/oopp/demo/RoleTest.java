7af2bcc41e40a30511977b73e45a8745e7e8e8e5
mihailsp <m.b.spasov@student.tudelft.nl>
2020-03-06 02:00:02 +0100

    Added RoleTest class and fixed inconsistencies in Javadoc.

---
+++ server/src/test/java/nl/tudelft/oopp/demo/RoleTest.java
@@ -0,0 +1,84 @@
+package nl.tudelft.oopp.demo;
+
+import nl.tudelft.oopp.demo.entities.Role;
+import nl.tudelft.oopp.demo.entities.User;
+import nl.tudelft.oopp.demo.repositories.RoleRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class RoleTest {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    Role role;
+    Role role2;
+
+    /** Sets up the classes before executing the tests.
+     */
+    @BeforeEach
+    public void setup() {
+        role = new Role();
+        role.setId(1);
+        role.setName("Tutor");
+        role.setUsers(new HashSet<>());
+        roleRepository.save(role);
+    }
+
+    /** Tests the constructor of the Role class
+     */
+    @Test
+    public void testConstructor(){
+        role2 = new Role();
+        assertNotNull(role);
+    }
+
+    /** Tests the getters of the Role class
+     */
+    @Test
+    public void testGetters(){
+        role2 = roleRepository.findAll().get(0);
+        Set<User> userSet = new HashSet<User>();
+        assertEquals(role.getId(), role2.getId());
+        assertEquals(role.getId(), 1);
+        assertEquals(role.getName(), role2.getName());
+        assertEquals(role2.getName(), "Tutor");
+    }
+
+    /** Tests the setters of the Role class
+     */
+    @Test
+    public void testSetters(){
+        Set<User> userSet = new HashSet<User>();
+        role2 = new Role();
+        role2.setId(7);
+        role2.setName("Manager");
+        role2.setUsers(userSet);
+        assertEquals(role2.getId(),7);
+        assertEquals(role2.getName(), "Manager");
+        assertEquals(role2.getUsers(), userSet);
+    }
+
+    /** Tests retrieving and saving data from the RoleRepository.
+     */
+    @Test
+    public void saveAndRetrieveUser() {
+        role2 = roleRepository.findAll().get(0);
+        assertTrue(role.getName() == role2.getName());
+    }
+
+    /** Deletes everything from the repositories after testing.
+     */
+    @AfterEach
+    public void cleanup() {
+        roleRepository.deleteAll();
+    }
+}
