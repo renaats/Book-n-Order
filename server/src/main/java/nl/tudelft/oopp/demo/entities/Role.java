1096056657db3455f62ae5d9baed5e58e9729cbc
mihailsp <m.b.spasov@student.tudelft.nl>
2020-03-06 02:23:05 +0100

    Removed setId method for less confusion.

--- server/src/main/java/nl/tudelft/oopp/demo/entities/Role.java
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/Role.java
@@ -16,10 +16,6 @@ public class Role {
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
