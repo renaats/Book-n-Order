0d0d708c189a704962dc09e5ce9f210b04490f83
Alex De Los Santos Subirats <a.delossantossubirats@student.tudelft.nl>
2020-03-07 17:50:24 +0100

    Added some methods that should work as derived queries for issue 11 (still need to be tried out)

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/BuildingRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/BuildingRepository.java
@@ -4,10 +4,15 @@ import nl.tudelft.oopp.demo.entities.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 @Repository
 public interface BuildingRepository extends JpaRepository<Building, Integer> {
+    List<Building> findByid(int id);
+    List<Building> findByname(String Name);
+    List<Building> findByroomsid(int id);
 }
