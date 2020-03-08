d64220e98b6a6e39cce19ace1eb00f27a96ddead
mihailsp <m.b.spasov@student.tudelft.nl>
2020-03-08 15:21:09 +0100

    Added some tests to RoleServiceTest.

--- server/src/test/java/nl/tudelft/oopp/demo/RoleServiceTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/RoleServiceTest.java
@@ -1,19 +1,25 @@
 package nl.tudelft.oopp.demo;
 
 import nl.tudelft.oopp.demo.entities.Role;
-import nl.tudelft.oopp.demo.services.BikeService;
-import nl.tudelft.oopp.demo.services.BikeServiceImpl;
 import nl.tudelft.oopp.demo.services.RoleService;
 import nl.tudelft.oopp.demo.services.RoleServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
-class RoleServiceTest {
+public class RoleServiceTest {
     @TestConfiguration
     static class RoleServiceTestConfiguration {
         @Bean
@@ -25,5 +31,23 @@ class RoleServiceTest {
     @Autowired
     RoleService roleService;
 
+    Role role;
 
+    @BeforeEach
+    public void setup() {
+        role = new Role();
+        role.setName("Manager");
+       // role.setUsers(new HashSet<User>());
+    }
+
+    @Test
+    public void testConstructor() {
+        assertNotNull(roleService);
+    }
+
+    @Test
+    public void testCreate() {
+        roleService.add(role.getName());
+        assertEquals(Arrays.asList(role), roleService.all());
+    }
 }
