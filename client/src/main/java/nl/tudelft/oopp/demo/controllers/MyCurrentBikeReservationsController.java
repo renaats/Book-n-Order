4f2b4970b705de2ccfc6af4f658fcaa7ea693327
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-13 07:56:26 +0100

    Chages to the scenes

---
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MyCurrentBikeReservationsController.java
@@ -0,0 +1,18 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import javafx.fxml.Initializable;
+import nl.tudelft.oopp.demo.communication.ServerCommunication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MyCurrentBikeReservationsController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadData();
+    }
+
+    public void loadData() {
+
+    }
+}
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/OrderFoodController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/OrderFoodController.java
@@ -2,12 +2,14 @@ package nl.tudelft.oopp.demo.controllers;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
 import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
 public class OrderFoodController {
 
     /**
      * Changes to myCurrentBookings.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void myCurrentBookings() throws IOException {
@@ -16,6 +18,7 @@ public class OrderFoodController {
 
     /**
      * Changes to myPreviousBookings.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void myPreviousBookings() throws IOException {
@@ -24,6 +27,7 @@ public class OrderFoodController {
 
     /**
      * Changes to myAccountScene.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void myAccountScene() throws IOException {
@@ -32,6 +36,7 @@ public class OrderFoodController {
 
     /**
      * Changes to bikeReservations.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void rentBike(ActionEvent actionEvent) throws IOException {
@@ -40,6 +45,7 @@ public class OrderFoodController {
 
     /**
      * Changes to bookRoom.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void bookRoom(ActionEvent actionEvent) throws IOException {
@@ -48,6 +54,7 @@ public class OrderFoodController {
 
     /**
      * Changes to orderFood.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void orderFood(ActionEvent actionEvent) throws IOException {
@@ -56,30 +63,41 @@ public class OrderFoodController {
 
     /**
      * Changes to mainMenuReservations.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void mainMenu(ActionEvent actionEvent) throws IOException {
-        ApplicationDisplay.changeScene("/mainMenuReservations.fxml");
+        ApplicationDisplay.changeScene("/mainMenu.fxml");
     }
 
     /**
-     * Changes to orderFoodRestaurantChoice.fxml.
+     * Changes to orderFood1.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void restaurantChoice(ActionEvent actionEvent) throws IOException {
-        ApplicationDisplay.changeScene("/orderFoodRestaurantChoice.fxml");
+        ApplicationDisplay.changeScene("/orderFood1.fxml");
     }
 
     /**
      * Changes to orderFoodAulaMenu.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void aulaMenu(ActionEvent actionEvent) throws IOException {
         ApplicationDisplay.changeScene("/DinamicRestaurant.fxml");
     }
-
+    /**
+     * return to the reservations menu when the back arrow button is clicked.
+     * @param mouseEvent The event tis the clicking of the arrow button
+     * @throws IOException the input will allways be the same, so it should never throw an IO exception
+     */
+    public void goToMainMenuReservations(MouseEvent mouseEvent) throws IOException {
+        ApplicationDisplay.changeScene("/mainMenuReservations.fxml");
+    }
     /**
      * Changes to orderFoodFoodTruckMenu.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void foodtruckMenu(ActionEvent actionEvent) throws IOException {
@@ -88,6 +106,7 @@ public class OrderFoodController {
 
     /**
      * Changes to orderFoodPulseMenu.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void pulseMenu(ActionEvent actionEvent) throws IOException {
@@ -96,6 +115,7 @@ public class OrderFoodController {
 
     /**
      * Changes to orderFoodCoffeeStarMenu.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void coffeestarMenu(ActionEvent actionEvent) throws IOException {
@@ -104,6 +124,7 @@ public class OrderFoodController {
 
     /**
      * Changes to orderFoodCafeXDinnerMenu.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void cafexMenu(ActionEvent actionEvent) throws IOException {
@@ -112,9 +133,11 @@ public class OrderFoodController {
 
     /**
      * Changes to orderFoodConfirmation.fxml.
+     *
      * @throws IOException input will not be wrong, hence we throw.
      */
     public void orderFoodConfirmation(ActionEvent actionEvent) throws IOException {
-        ApplicationDisplay.changeScene("/orderFoodConfirmation.fxml");
+        ApplicationDisplay.changeScene("/FoodConfirmation.fxml");
     }
+
 }
