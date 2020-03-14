a9405caad808dc102420b82c8695d2c119eedd20
Alto Delia <A.delia@student.tudelft.nl>
2020-03-14 15:56:56 +0100

    Added JSON parser for Room Reservation entity

--- client/src/main/java/nl/tudelft/oopp/demo/communication/JsonMapper.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/JsonMapper.java
@@ -4,13 +4,13 @@ import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-import java.util.List;
-
 import javafx.scene.control.Alert;
 import nl.tudelft.oopp.demo.entities.Building;
 import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.entities.RoomReservation;
+
+import java.io.IOException;
+import java.util.List;
 
 public class JsonMapper {
 
@@ -101,6 +101,52 @@ public class JsonMapper {
             alert.setTitle("Error");
             alert.setHeaderText(null);
             alert.setContentText(roomsJson);
+            alert.showAndWait();
+        }
+        return null;
+    }
+
+    /**
+     * Maps JSON to RoomReservation entity.
+     * @param roomReservationJson representation of a room.
+     * @return Room entity.
+     */
+    public static RoomReservation roomReservationMapper(String roomReservationJson) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            // Convert JSON string to Object
+            RoomReservation roomReservation = mapper.readValue(roomReservationJson, RoomReservation.class);
+            return roomReservation;
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText(roomReservationJson);
+            alert.showAndWait();
+        }
+        return null;
+    }
+
+    /**
+     * Maps all Room Reservation JSONS to a list.
+     * @param roomReservationsJson a JSON string representing a list.
+     * @return A list filled with object Room Reservation
+     */
+    public static List<RoomReservation> roomReservationsListMapper(String roomReservationsJson) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            // Convert JSON string to Object
+            List<RoomReservation> allRoomReservations = mapper.readValue(roomReservationsJson, new TypeReference<List<RoomReservation>>(){});
+            return allRoomReservations;
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText(roomReservationsJson);
             alert.showAndWait();
         }
         return null;
