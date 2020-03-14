808f4c60ab063177c56449f8e866ee8a0534fd66
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-14 10:38:06 +0100

    deleted some un suded methods

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainMenuController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainMenuController.java
@@ -13,7 +13,7 @@ public class MainMenuController {
      * Handels the clicking of the calendar icon.
      * @throws IOException when it fails
      */
-    public void calendarIcon(MouseEvent mouseEvent) throws IOException {
+    public void calendarIcon() throws IOException {
         ApplicationDisplay.changeScene("/calendar.fxml");
     public void calendarIcon() throws IOException {
         ApplicationDisplay.changeScene("/calendar.fxml");
@@ -23,28 +23,12 @@ public class MainMenuController {
      * @throws IOException when it fails
      */
 
-    public void goToProfile(MouseEvent mouseEvent) throws IOException {
+    public void goToProfile() throws IOException {
         ApplicationDisplay.changeScene("/myAccountScene.fxml");
     }
     /**
      * Handels the clicking of the Bookings icon.
      * @throws IOException when it fails
-     */
-
-    public void goToMainReservationsMenu(MouseEvent mouseEvent) throws IOException {
-        ApplicationDisplay.changeScene("/MainMenuReservations.fxml");
-    }
-
-
-
-
-    public void goToProfile() throws IOException {
-        ApplicationDisplay.changeScene("/myAccountScene.fxml");
-    }
-
-    /**
-     * Changes current scene to myCurrentBookings.fxml.
-     * @throws IOException input will not be wrong, hence we throw.
      */
 
     public void goToMainReservationsMenu() throws IOException {
@@ -56,20 +40,6 @@ public class MainMenuController {
      * @param actionEvent
      * @throws IOException
      */
-    public void template(ActionEvent actionEvent) throws IOException {
-        ApplicationDisplay.changeScene("/Template.fxml");
-    }
-
-    public void calendarIcon(MouseEvent mouseEvent) {
-
-    }
-
-    public void myCurrentBookings(ActionEvent actionEvent) {
-    }
-
-    public void myPreviousBookings(ActionEvent actionEvent) {
-    }
-
     public void template(ActionEvent actionEvent) throws IOException {
         ApplicationDisplay.changeScene("/Template.fxml");
     }
