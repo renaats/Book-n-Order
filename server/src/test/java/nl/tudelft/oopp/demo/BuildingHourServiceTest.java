a80aee283c60271f1648aaf79b879193cccfb697
rjursevskis <r.jursevskis@student.tudelft.nl>
2020-03-14 20:09:22 +0100

    Split up all tests to have at most 2 assertions and added JavaDocs

--- server/src/test/java/nl/tudelft/oopp/demo/BuildingHourServiceTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/BuildingHourServiceTest.java
@@ -12,10 +12,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+
 import nl.tudelft.oopp.demo.entities.Building;
 import nl.tudelft.oopp.demo.entities.BuildingHours;
 import nl.tudelft.oopp.demo.services.BuildingHourService;
 import nl.tudelft.oopp.demo.services.BuildingService;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,7 +30,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class BuildingHourServiceTest {
-
     @TestConfiguration
     static class BuildingServiceTestConfiguration {
         @Bean
@@ -57,7 +58,8 @@ public class BuildingHourServiceTest {
     Building building;
     Building building2;
 
-    /** Sets up the classes before executing the tests.
+    /**
+     * Sets up the entities and saves them via a service before executing every test.
      */
     @BeforeEach
     public void setup() {
@@ -81,89 +83,276 @@ public class BuildingHourServiceTest {
         buildingHours2 = new BuildingHours(2, building2, LocalTime.ofSecondOfDay(3000), LocalTime.ofSecondOfDay(4000));
     }
 
+    /**
+     * Tests the constructor creating a new instance of the entity.
+     */
     @Test
     public void testConstructor() {
         assertNotNull(buildingHourService);
     }
 
+    /**
+     * Tests the creation of an instance with an invalid building id.
+     */
     @Test
-    public void testCreate() {
+    public void testInvalidBuilding() {
+        assertEquals(422, buildingHourService.add(0, buildingHours.getDay(), 1000, 3000));
+    }
+
+    /**
+     * Tests the creation of an instance with a day not between 1 and 7.
+     */
+    @Test
+    public void testInvalidDay() {
+        assertEquals(425, buildingHourService.add(building.getId(), 8, 1000, 3000));
+    }
+
+    /**
+     * Tests the creation of an instance with the end time before the start time.
+     */
+    @Test
+    public void testEndBeforeStart() {
+        assertEquals(426, buildingHourService.add(building.getId(), buildingHours.getDay(), 3000, 1000));
+    }
+
+    /**
+     * Tests the creation of an instance that already exists.
+     */
+    @Test
+    public void testSameBuildingTwice() {
         assertEquals(201, buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000));
+        assertEquals(427, buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000));
+    }
+
+    /**
+     * Tests the saving and retrieval of an instance of BuildingHourService.
+     */
+    @Test
+    public void testRetrieveOne() {
+        assertEquals(201, buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000));
+
         assertEquals(Collections.singletonList(buildingHours), buildingHourService.all());
     }
 
+    /**
+     * Tests the iterator of an empty list of BuildingHours.
+     */
     @Test
-    public void testAll() {
+    public void testEmptyIterator() {
         Iterator<BuildingHours> iterator = buildingHourService.all().iterator();
         assertFalse(iterator.hasNext());
+    }
+
+    /**
+     * Tests the iterator of a non-empty list of BuildingHours.
+     */
+    @Test
+    public void testNonEmptyIterator() {
         assertEquals(201, buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000));
-        iterator = buildingHourService.all().iterator();
+
+        Iterator<BuildingHours> iterator = buildingHourService.all().iterator();
         assertTrue(iterator.hasNext());
         iterator.next();
         assertFalse(iterator.hasNext());
     }
 
+    /**
+     * Tests the search for a non-existing object.
+     */
     @Test
-    public void testFind() {
+    public void testFindNonExisting() {
         assertEquals(201, buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000));
+
         buildingHours = buildingHourService.all().iterator().next();
-        assertEquals(buildingHours, buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()));
         assertNull(buildingHourService.find(5, 1));
     }
 
+    /**
+     * Tests the retrieval of multiple instances of BuildingHours.
+     */
     @Test
-    public void testUpdate() {
-        assertEquals(201, buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000));
-        assertEquals(201, buildingHourService.add(building2.getId(), buildingHours2.getDay(), 2000, 4000));
+    public void testRetrieveMultiple() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 2000, 4000);
 
         List<BuildingHours> buildingHourList = new ArrayList<>();
         buildingHourService.all().forEach(buildingHourList::add);
         assertEquals(2, buildingHourList.size());
+
+    }
+
+    /**
+     * Tests the update operation on a non-existent object.
+     */
+    @Test
+    public void testUpdateNonExisting() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 2000, 4000);
+
         assertEquals(416, buildingHourService.update(1234, "attr", "val"));
+    }
+
+    /**
+     * Tests the change of the day by using the service.
+     */
+    @Test
+    public void testChangeDay() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 1000, 3000);
+
+        List<BuildingHours> buildingHourList = new ArrayList<>();
+        buildingHourService.all().forEach(buildingHourList::add);
         buildingHours = buildingHourList.get(0);
         buildingHours2 = buildingHourList.get(1);
 
         assertNotEquals(buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()),
                 buildingHourService.find(buildingHours2.getBuilding().getId(), buildingHours2.getDay()));
 
-        assertEquals(201, buildingHourService.update(buildingHours2.getId(), "starttimes",
-                ((Integer) buildingHours.getStartTime().toSecondOfDay()).toString()));
+        buildingHourService.update(buildingHours2.getId(), "day", ((Integer) buildingHours.getDay()).toString());
+
+        assertEquals(buildingHours.getDay(), buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()).getDay());
+    }
+
+    /**
+     * Tests the change of the building by using the service.
+     */
+    @Test
+    public void testChangeBuilding() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 1000, 3000);
+
+        List<BuildingHours> buildingHourList = new ArrayList<>();
+        buildingHourService.all().forEach(buildingHourList::add);
+        buildingHours = buildingHourList.get(0);
+        buildingHours2 = buildingHourList.get(1);
 
         assertNotEquals(buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()),
                 buildingHourService.find(buildingHours2.getBuilding().getId(), buildingHours2.getDay()));
 
-        assertEquals(201, buildingHourService.update(buildingHours2.getId(), "endtimes",
-                ((Integer) buildingHours.getEndTime().toSecondOfDay()).toString()));
+        buildingHourService.update(buildingHours2.getId(), "buildingid", ((Integer) buildingHours.getBuilding().getId()).toString());
+
+        assertEquals(building, buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()).getBuilding());
+    }
+
+    /**
+     * Tests the change of the start time by using the service.
+     */
+    @Test
+    public void testChangeStartTime() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 2000, 3000);
+
+        List<BuildingHours> buildingHourList = new ArrayList<>();
+        buildingHourService.all().forEach(buildingHourList::add);
+        buildingHours = buildingHourList.get(0);
+        buildingHours2 = buildingHourList.get(1);
 
         assertNotEquals(buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()),
                 buildingHourService.find(buildingHours2.getBuilding().getId(), buildingHours2.getDay()));
 
-        assertEquals(201, buildingHourService.update(buildingHours2.getId(), "buildingid",
-                ((Integer) buildingHours.getBuilding().getId()).toString()));
+        buildingHourService.update(buildingHours2.getId(), "starttimes", ((Integer) buildingHours.getStartTime().toSecondOfDay()).toString());
+
+        assertEquals(buildingHours.getStartTime(),
+                buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()).getStartTime());
+    }
+
+    /**
+     * Tests the change of the end time by using the service.
+     */
+    @Test
+    public void testChangeEndTime() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 1000, 4000);
+
+        List<BuildingHours> buildingHourList = new ArrayList<>();
+        buildingHourService.all().forEach(buildingHourList::add);
+        buildingHours = buildingHourList.get(0);
+        buildingHours2 = buildingHourList.get(1);
 
         assertNotEquals(buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()),
                 buildingHourService.find(buildingHours2.getBuilding().getId(), buildingHours2.getDay()));
+
+        buildingHourService.update(buildingHours2.getId(), "endtimes", ((Integer) buildingHours.getEndTime().toSecondOfDay()).toString());
+
+        assertEquals(buildingHours.getEndTime(), buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()).getEndTime());
+    }
+
+    /**
+     * Tests the rejection of the modification of an instance that would make it equal to another instance.
+     */
+    @Test
+    public void testRejectEqualInstances() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 1000, 3000);
+
+        List<BuildingHours> buildingHourList = new ArrayList<>();
+        buildingHourService.all().forEach(buildingHourList::add);
+        buildingHours = buildingHourList.get(0);
+        buildingHours2 = buildingHourList.get(1);
+
+        assertNotEquals(buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()),
+                buildingHourService.find(buildingHours2.getBuilding().getId(), buildingHours2.getDay()));
+
+        buildingHourService.update(buildingHours2.getId(), "buildingid", ((Integer) buildingHours.getBuilding().getId()).toString());
 
         assertEquals(427, buildingHourService.update(buildingHours2.getId(), "day", ((Integer) buildingHours.getDay()).toString()));
     }
 
+    /**
+     * Tests the deletion of an instance.
+     */
     @Test
-    public void testDelete() {
-        assertEquals(201, buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000));
-        assertEquals(201, buildingHourService.add(building2.getId(), buildingHours2.getDay(), 2000, 4000));
+    public void testDeleteOne() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 2000, 4000);
 
         List<BuildingHours> buildingHourList = new ArrayList<>();
         buildingHourService.all().forEach(buildingHourList::add);
-        assertEquals(2, buildingHourList.size());
         buildingHours = buildingHourList.get(0);
         buildingHours2 = buildingHourList.get(1);
+
         buildingHourService.delete(buildingHourList.get(0).getBuilding().getId(), buildingHourList.get(0).getDay());
 
         buildingHourList = new ArrayList<>();
         buildingHourService.all().forEach(buildingHourList::add);
         assertEquals(1, buildingHourList.size());
+    }
+
+    /**
+     * Tests the deletion of the correct instance.
+     */
+    @Test
+    public void testDeleteSearch() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 2000, 4000);
+
+        List<BuildingHours> buildingHourList = new ArrayList<>();
+        buildingHourService.all().forEach(buildingHourList::add);
+        buildingHours = buildingHourList.get(0);
+        buildingHours2 = buildingHourList.get(1);
+
+        buildingHourService.delete(buildingHourList.get(0).getBuilding().getId(), buildingHourList.get(0).getDay());
+
+        buildingHourList = new ArrayList<>();
+        buildingHourService.all().forEach(buildingHourList::add);
         assertNull(buildingHourService.find(buildingHours.getBuilding().getId(), buildingHours.getDay()));
         assertNotNull(buildingHourService.find(buildingHours2.getBuilding().getId(), buildingHours2.getDay()));
     }
 
+
+    /**
+     * Tests the deletion of a non-existent instance.
+     */
+    @Test
+    public void testDeleteNonExistent() {
+        buildingHourService.add(building.getId(), buildingHours.getDay(), 1000, 3000);
+        buildingHourService.add(building2.getId(), buildingHours2.getDay(), 2000, 4000);
+
+        List<BuildingHours> buildingHourList = new ArrayList<>();
+        buildingHourService.all().forEach(buildingHourList::add);
+        buildingHours = buildingHourList.get(0);
+        buildingHours2 = buildingHourList.get(1);
+
+        assertEquals(404, buildingHourService.delete(buildingHourList.get(0).getBuilding().getId(), buildingHourList.get(0).getDay() + 1));
+    }
 }
--- server/src/test/java/nl/tudelft/oopp/demo/RestaurantHourTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/RestaurantHourTest.java
@@ -2,18 +2,16 @@ package nl.tudelft.oopp.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 import java.time.LocalTime;
 
 import nl.tudelft.oopp.demo.entities.Building;
-import nl.tudelft.oopp.demo.entities.BuildingHours;
 import nl.tudelft.oopp.demo.entities.Restaurant;
 import nl.tudelft.oopp.demo.entities.RestaurantHours;
-import nl.tudelft.oopp.demo.repositories.BuildingHourRepository;
 import nl.tudelft.oopp.demo.repositories.BuildingRepository;
 import nl.tudelft.oopp.demo.repositories.RestaurantHourRepository;
 import nl.tudelft.oopp.demo.repositories.RestaurantRepository;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,7 +34,7 @@ public class RestaurantHourTest {
     RestaurantHours restaurantHours2;
 
     /**
-     * Sets up the classes before executing the tests.
+     * Sets up the entities and saves them in the repository before executing every test.
      */
     @BeforeEach
     public void setup() {
@@ -60,42 +58,113 @@ public class RestaurantHourTest {
         restaurantHourRepository.save(restaurantHours);
     }
 
+    /**
+     * Tests the saving and retrieval of an instance of restaurantHours.
+     */
     @Test
     public void saveAndRetrieveBuildingHours() {
         restaurantHours2 = restaurantHourRepository.findByRestaurant_IdAndDay(restaurant.getId(), 1);
         assertEquals(restaurantHours, restaurantHours2);
     }
 
+    /**
+     * Tests the getter for the day field.
+     */
     @Test
-    public void testGetters() {
+    public void testDayGetter() {
         restaurantHours2 = restaurantHourRepository.findByRestaurant_IdAndDay(restaurant.getId(), 1);
         assertEquals(1, restaurantHours2.getDay());
-        assertEquals(restaurant, restaurantHours2.getRestaurant());
-        assertEquals(LocalTime.ofSecondOfDay(1000), restaurantHours2.getStartTime());
-        assertEquals(LocalTime.ofSecondOfDay(3000), restaurantHours2.getEndTime());
-    }
-
-    @Test
-    public void testEqualBuildingHours() {
-        restaurantHours2 = new RestaurantHours();
-        restaurantHours2.setDay(2);
-        restaurantHours2.setRestaurant(restaurant2);
-        restaurantHours2.setStartTime(LocalTime.ofSecondOfDay(2000));
-        restaurantHours2.setEndTime(LocalTime.ofSecondOfDay(4000));
-        assertNotEquals(restaurantHours, restaurantHours2);
-        restaurantHours2.setDay(1);
-        assertNotEquals(restaurantHours, restaurantHours2);
-        restaurantHours2.setRestaurant(restaurant);
-        assertNotEquals(restaurantHours, restaurantHours2);
-        restaurantHours2.setStartTime(LocalTime.ofSecondOfDay(1000));
-        assertNotEquals(restaurantHours, restaurantHours2);
-        restaurantHours2.setEndTime(LocalTime.ofSecondOfDay(3000));
-        assertEquals(restaurantHours, restaurantHours2);
-        assertNotSame(restaurantHours, restaurantHours2);
     }
 
     /**
-     * Cleans up the classes after executing the tests.
+     * Tests the getter for the restaurant field.
+     */
+    @Test
+    public void testRestaurantGetter() {
+        restaurantHours2 = restaurantHourRepository.findByRestaurant_IdAndDay(restaurant.getId(), 1);
+        assertEquals(restaurant, restaurantHours2.getRestaurant());
+    }
+
+    /**
+     * Tests the getter for the startTime field.
+     */
+    @Test
+    public void testStartTimeGetter() {
+        restaurantHours2 = restaurantHourRepository.findByRestaurant_IdAndDay(restaurant.getId(), 1);
+        assertEquals(LocalTime.ofSecondOfDay(1000), restaurantHours2.getStartTime());
+    }
+
+    /**
+     * Tests the getter for the endTime field.
+     */
+    @Test
+    public void testEndTimeGetters() {
+        restaurantHours2 = restaurantHourRepository.findByRestaurant_IdAndDay(restaurant.getId(), 1);
+        assertEquals(LocalTime.ofSecondOfDay(3000), restaurantHours2.getEndTime());
+    }
+
+    /**
+     * Tests the the change of the day by using a setter.
+     */
+    @Test
+    public void testChangeDay() {
+        restaurantHours2 = new RestaurantHours();
+        restaurantHours2.setDay(2);
+        restaurantHours2.setRestaurant(restaurant);
+        restaurantHours2.setStartTime(LocalTime.ofSecondOfDay(1000));
+        restaurantHours2.setEndTime(LocalTime.ofSecondOfDay(3000));
+        assertNotEquals(restaurantHours, restaurantHours2);
+        restaurantHours2.setDay(1);
+        assertEquals(restaurantHours, restaurantHours2);
+    }
+
+    /**
+     * Tests the the change of the restaurant by using a setter.
+     */
+    @Test
+    public void testChangeRestaurant() {
+        restaurantHours2 = new RestaurantHours();
+        restaurantHours2.setDay(1);
+        restaurantHours2.setRestaurant(restaurant2);
+        restaurantHours2.setStartTime(LocalTime.ofSecondOfDay(1000));
+        restaurantHours2.setEndTime(LocalTime.ofSecondOfDay(3000));
+        assertNotEquals(restaurantHours, restaurantHours2);
+        restaurantHours2.setRestaurant(restaurant);
+        assertEquals(restaurantHours, restaurantHours2);
+    }
+
+    /**
+     * Tests the the change of the start time by using a setter.
+     */
+    @Test
+    public void testChangeStartTime() {
+        restaurantHours2 = new RestaurantHours();
+        restaurantHours2.setDay(1);
+        restaurantHours2.setRestaurant(restaurant);
+        restaurantHours2.setStartTime(LocalTime.ofSecondOfDay(2000));
+        restaurantHours2.setEndTime(LocalTime.ofSecondOfDay(3000));
+        assertNotEquals(restaurantHours, restaurantHours2);
+        restaurantHours2.setStartTime(LocalTime.ofSecondOfDay(1000));
+        assertEquals(restaurantHours, restaurantHours2);
+    }
+
+    /**
+     * Tests the the change of the ent time by using a setter.
+     */
+    @Test
+    public void testChangeEndTime() {
+        restaurantHours2 = new RestaurantHours();
+        restaurantHours2.setDay(1);
+        restaurantHours2.setRestaurant(restaurant);
+        restaurantHours2.setStartTime(LocalTime.ofSecondOfDay(1000));
+        restaurantHours2.setEndTime(LocalTime.ofSecondOfDay(4000));
+        assertNotEquals(restaurantHours, restaurantHours2);
+        restaurantHours2.setEndTime(LocalTime.ofSecondOfDay(3000));
+        assertEquals(restaurantHours, restaurantHours2);
+    }
+
+    /**
+     * Cleans up the repositories after executing every test.
      */
     @AfterEach
     public void cleanup() {
