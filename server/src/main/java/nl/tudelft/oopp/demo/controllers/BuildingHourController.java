717cdf52eb0ebac325985d8b4c364cbbb406be46
Renāts Jurševskis <r.jursevskis@student.tudelft.nl>
2020-03-14 11:57:50 +0100

    Removed empty lines

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/BuildingHourController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/BuildingHourController.java
@@ -7,6 +7,7 @@ import nl.tudelft.oopp.demo.entities.BuildingHours;
 import nl.tudelft.oopp.demo.entities.Room;
 import nl.tudelft.oopp.demo.services.BuildingHourService;
 import nl.tudelft.oopp.demo.services.BuildingService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Repository;
@@ -18,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @Repository
 @RestController
