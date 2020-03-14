397ee23661effc15336a73c5b5582f2c5700a2e9
Alto Delia <A.delia@student.tudelft.nl>
2020-03-14 18:36:44 +0100

    Removed badly implemented method and entered code into start method

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -1,13 +1,12 @@
 package nl.tudelft.oopp.demo;
 
-import nl.tudelft.oopp.demo.views.ApplicationDisplay;
-import nl.tudelft.oopp.demo.views.MyCalendarView;
+import nl.tudelft.oopp.demo.views.RoomCalendarView;
 
 public class MainApp {
     /**
      * runs the client side app
      */
     public static void main(String[] args) {
-        MyCalendarView.main(new String[0]);
+        RoomCalendarView.main(new String[0]);
     }
 }
