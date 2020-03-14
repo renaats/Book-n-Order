33e52ec69aa362cc459895920d384b0fd5a9e2a4
Alto Delia <A.delia@student.tudelft.nl>
2020-03-14 17:28:11 +0100

    Added method that creates calendar entries from DB

--- client/src/main/java/nl/tudelft/oopp/demo/views/MyCalendarView.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/MyCalendarView.java
@@ -1,27 +1,34 @@
 package nl.tudelft.oopp.demo.views;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.Calendar.Style;
-import com.calendarfx.model.CalendarEvent;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.model.Entry;
-import com.calendarfx.view.*;
-
+import com.calendarfx.view.CalendarView;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import nl.tudelft.oopp.demo.communication.JsonMapper;
+import nl.tudelft.oopp.demo.communication.ServerCommunication;
+import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.entities.RoomReservation;
+
+import java.time.*;
+import java.util.Date;
+import java.util.List;
 
 public class MyCalendarView extends Application {
 
+    private static Room room;
+
+    public MyCalendarView(Room room){
+        this.room = room;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        CalendarView weekView = new CalendarView ();
-        Calendar booked_rooms = new Calendar("Booked Rooms");
+        CalendarView weekView = new CalendarView();
         weekView.setShowSearchField(false);
         weekView.setShowPrintButton(false);
         weekView.setShowPageToolBarControls(false);
@@ -30,16 +37,17 @@ public class MyCalendarView extends Application {
         weekView.setShowSourceTray(false);
         weekView.setFocusTraversable(false);
 
-        EventHandler<CalendarEvent> loadEntries = e -> addEntries (e);
-        //weekView.addEventHandler(loadEntries);
+        Calendar booked_rooms = new Calendar("Booked Rooms");
+
+//        EventHandler<CalendarEvent> loadEntries = e -> addEntries (e);
+//        //weekView.addEventHandler(loadEntries);
 
         booked_rooms.setStyle(Style.STYLE3);
 
-        CalendarSource myCalendarSource = new CalendarSource("My Calendars");
+        CalendarSource myCalendarSource = new CalendarSource("Calendars");
         myCalendarSource.getCalendars().addAll(booked_rooms);
 
         weekView.getCalendarSources().addAll(myCalendarSource);
-
         weekView.setRequestedTime(LocalTime.now());
 
         Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
@@ -59,7 +67,9 @@ public class MyCalendarView extends Application {
                     }
 
                 }
-            };
+            }
+
+            ;
         };
 
         updateTimeThread.setPriority(Thread.MIN_PRIORITY);
@@ -77,12 +87,32 @@ public class MyCalendarView extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
-    private static void addEntries (CalendarEvent e){
-        Entry entry = e.getEntry();
-        LocalTime startTime = entry.getStartTime();
+    public void addEntries() {
+        List<RoomReservation> roomReservationList = JsonMapper.roomReservationsListMapper(ServerCommunication.getRoomReservations());
+
+        for (RoomReservation reservation : roomReservationList) {
+            if (reservation.getRoom().equals(this.room)) {
+                Entry<RoomReservation> bookedEntry = new Entry<>("Room is booked or unavailable");
+
+                Date start = reservation.getFromTime();
+                Instant instant1 = Instant.ofEpochMilli(start.getTime());
+                LocalTime startTime = LocalDateTime.ofInstant(instant1, ZoneId.systemDefault()).toLocalTime();
+
+                Date end = reservation.getToTime();
+                Instant instant2 = Instant.ofEpochMilli(end.getTime());
+                LocalTime endTime = LocalDateTime.ofInstant(instant2, ZoneId.systemDefault()).toLocalTime();
+
+                Instant instant3 = Instant.ofEpochMilli(start.getDate());
+                LocalDate date = LocalDateTime.ofInstant(instant3, ZoneId.systemDefault()).toLocalDate();
+
+                bookedEntry.setInterval(startTime, endTime);
+                bookedEntry.setInterval(date);
+            }
+        }
+
     }
 }
 
