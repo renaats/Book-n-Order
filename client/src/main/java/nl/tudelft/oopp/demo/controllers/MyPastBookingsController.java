82e7dbe9e92e8dfe34bf71a53e2d83ff4c96abd7
Alto Delia <A.delia@student.tudelft.nl>
2020-03-15 17:16:38 +0100

    SoruceTree

---
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MyPastBookingsController.java
@@ -0,0 +1,70 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import java.io.IOException;
+import nl.tudelft.oopp.demo.views.ApplicationDisplay;
+
+public class MyPastBookingsController {
+    /**
+     * Changes to myAccountScene.fxml.
+     * @throws IOException input will be valid.
+     */
+    public void myAccountScene() throws IOException {
+        ApplicationDisplay.changeScene("/myAccountScene.fxml");
+    }
+
+    /**
+     * Changes to myCurrentBookings.fxml.
+     * @throws IOException input will be valid.
+     */
+    public void myCurrentBookings() throws IOException {
+        ApplicationDisplay.changeScene("/myCurrentBookings.fxml");
+    }
+
+    /**
+     * Changes to myPreviousBookings.fxml.
+     * @throws IOException input will be valid.
+     */
+    public void myPreviousBookings() throws IOException {
+        ApplicationDisplay.changeScene("/myPreviousBookings.fxml");
+    }
+
+    /**
+     * Changes to myPreviousBikeReservations.fxml.
+     * @throws IOException input will be valid.
+     */
+    public void myPreviousBikeReservations() throws IOException {
+        ApplicationDisplay.changeScene("/myPreviousTenBikeReservations.fxml");
+    }
+
+    /**
+     * Changes to myPreviousRoomBookings.fxml.
+     * @throws IOException input will be valid.
+     */
+    public void myPreviousRoomBookings() throws IOException {
+        ApplicationDisplay.changeScene("/myPreviousTenRoomBookings.fxml");
+    }
+
+    /**
+     * Changes to myPreviousFoodOrders.fxml.
+     * @throws IOException input will be valid.
+     */
+    public void myPreviousFoodOrders() throws IOException {
+        ApplicationDisplay.changeScene("/myPreviousTenFoodOrders.fxml");
+    }
+
+    /**
+     *  method changes the view to that of the main menu
+     * @throws IOException the method will never throw an exception
+     */
+    public void mainMenu() throws IOException {
+        ApplicationDisplay.changeScene("/mainMenu.fxml");
+    }
+
+    /**
+     * method changes view to main menu
+     * @throws IOException should never be thrown
+     */
+    public void goToMyAccount() throws IOException {
+        ApplicationDisplay.changeScene("/myAccountScene.fxml");
+    }
+}
---
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MyPreviousRoomReservationsController.java
@@ -0,0 +1,39 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import nl.tudelft.oopp.demo.views.ApplicationDisplay;
+
+
+public class MyPreviousRoomReservationsController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadData();
+    }
+
+    /**
+     * will load the previous reservations
+     */
+    public void loadData() {
+        //TODO
+    }
+
+    /**
+     * change the view to the main menu when the home icon is clicked
+     * @throws IOException should never be thrown as the input is always the same
+     */
+    public void mainMenu() throws IOException {
+        ApplicationDisplay.changeScene("/mainMenu.fxml");
+    }
+
+    /**
+     * changes view to the my Previous bookings view when the back arrow is clicked
+     * @throws IOException should never be thrown as the input is always the same
+     */
+    public void goToMyCurrentReservations() throws IOException {
+        ApplicationDisplay.changeScene("/myPreviousBookings.fxml");
+    }
+}
