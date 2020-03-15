cb9958fa2b027dc1246cb9399ccc48641d3bda38
ljongejans <l.j.jongejans@student.tudelft.nl>
2020-03-15 13:19:59 +0100

    FoodOrderRepository merge conflict

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/BuildingRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/BuildingRepository.java
@@ -11,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BuildingRepository extends JpaRepository<Building, Integer> {
     Building findById(String building);
+    Building findByName(String name);
 }
