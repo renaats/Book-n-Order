a4f18f4dbeecbe21347fde0fe63765acb021bd8e
Alto Delia <A.delia@student.tudelft.nl>
2020-03-16 21:56:35 +0100

    Fixed some checkstyle issues

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -6,6 +6,7 @@ public class MainApp {
     /**
      * runs the client side app
      */
-    public static void main(String[] args) { RoomCalendarView.main(new String[0]);
+    public static void main(String[] args) {
+        RoomCalendarView.main(new String[0]);
     }
 }
