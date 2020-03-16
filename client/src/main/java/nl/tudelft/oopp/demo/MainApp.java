f26a31c14e1ee6bc56a6b33554fd1039e694765e
Alto Delia <A.delia@student.tudelft.nl>
2020-03-16 21:56:35 +0100

    Fixed some checkstyle issues

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -3,10 +3,10 @@ package nl.tudelft.oopp.demo;
 import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
 public class MainApp {
+    
     /**
      * runs the client side app
      */
-
     public static void main(String[] args) {
         ApplicationDisplay.main(new String[0]);
     }
