a28d6828abfbb5b0f38584fd052d2951ea789083
Alto Delia <A.delia@student.tudelft.nl>
2020-03-17 15:33:20 +0100

    Chekstyle Warnings

--- client/src/main/java/nl/tudelft/oopp/demo/communication/JsonMapper.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/JsonMapper.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.util.List;
-
 import javafx.scene.control.Alert;
-import nl.tudelft.oopp.demo.entities.AppUser;
 import nl.tudelft.oopp.demo.entities.Building;
 import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.entities.RoomReservation;
+
 
 public class JsonMapper {
 
@@ -108,25 +107,49 @@ public class JsonMapper {
     }
 
     /**
-     * Maps a JSON string to an AppUser object
-     * @param roomJson JSON representation of a String
-     * @return AppUser Object
+     * Maps JSON to RoomReservation entity.
+     * @param roomReservationJson representation of a room.
+     * @return Room entity.
      */
-    public static AppUser appUserMapper(String roomJson) {
+    public static RoomReservation roomReservationMapper(String roomReservationJson) {
 
         ObjectMapper mapper = new ObjectMapper();
 
         try {
             // Convert JSON string to Object
-            AppUser appUser = mapper.readValue(roomJson, AppUser.class);
-            return appUser;
+            RoomReservation roomReservation = mapper.readValue(roomReservationJson, RoomReservation.class);
+            return roomReservation;
         } catch (Exception e) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Error");
             alert.setHeaderText(null);
-            alert.setContentText(roomJson);
+            alert.setContentText(roomReservationJson);
             alert.showAndWait();
         }
         return null;
     }
+
+    /**
+     * Maps all Room Reservation JSONS to a list.
+     * @param roomReservationsJson a JSON string representing a list.
+     * @return A list filled with object Room Reservation
+     */
+    public static List<RoomReservation> roomReservationsListMapper(String roomReservationsJson) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            // Convert JSON string to Object
+            List<RoomReservation> allRoomReservations = mapper.readValue(roomReservationsJson, new TypeReference<List<RoomReservation>>(){});
+            return allRoomReservations;
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText(roomReservationsJson);
+            alert.showAndWait();
+        }
+        return null;
+    }
+
 }
--- client/src/main/java/nl/tudelft/oopp/demo/views/RoomCalendarView.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/RoomCalendarView.java
@@ -2,13 +2,18 @@ package nl.tudelft.oopp.demo.views;
 
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.Calendar.Style;
-import com.calendarfx.model.CalendarEvent;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.model.Entry;
 import com.calendarfx.view.CalendarView;
+import java.time.LocalDate;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.communication.JsonMapper;
@@ -16,13 +21,6 @@ import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.entities.Room;
 import nl.tudelft.oopp.demo.entities.RoomReservation;
 
-import java.time.LocalTime;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.List;
 
 public class RoomCalendarView extends Application {
 
@@ -31,14 +29,14 @@ public class RoomCalendarView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        CalendarView weekView = new CalendarView();
-        weekView.setShowSearchField(false);
-        weekView.setShowPrintButton(false);
-        weekView.setShowPageToolBarControls(false);
-        weekView.setShowPageSwitcher(true);
-        weekView.setShowAddCalendarButton(false);
-        weekView.setShowSourceTray(false);
-        weekView.setFocusTraversable(false);
+        CalendarView roomCal = new CalendarView();
+        roomCal.setShowSearchField(false);
+        roomCal.setShowPrintButton(false);
+        roomCal.setShowPageToolBarControls(false);
+        roomCal.setShowPageSwitcher(true);
+        roomCal.setShowAddCalendarButton(false);
+        roomCal.setShowSourceTray(false);
+        roomCal.setFocusTraversable(false);
 
         Calendar bookedSlotsCalendar = new Calendar("Unavailable Slots"); //calendar that stores reserved slot entries
         bookedSlotsCalendar.setStyle(Style.STYLE2); //sets color of calendar to blue
@@ -46,41 +44,35 @@ public class RoomCalendarView extends Application {
 
         Calendar myBookingCalendar = new Calendar("My Bookings");
         myBookingCalendar.setStyle(Style.STYLE1);
-        EventHandler<CalendarEvent> handler = e -> entryHandler(e);
-        myBookingCalendar.addEventHandler(handler);
-
         List<RoomReservation> roomReservationList = JsonMapper.roomReservationsListMapper(ServerCommunication.getRoomReservations());
-                if (roomReservationList != null && !roomReservationList.isEmpty()) {
-                    for (RoomReservation reservation : roomReservationList) {
-                        if (reservation.getRoom().equals(this.room)) {
-                            Entry<RoomReservation> bookedEntry = new Entry<>("Room is booked or unavailable");
-
-                            LocalTime startTime = convertToLocalTime(reservation.getFromTime());
-                            LocalTime endTime = convertToLocalTime(reservation.getToTime());
-                            LocalDate date = convertToLocalDate(reservation.getFromTime());
-
-                            bookedEntry.setInterval(startTime, endTime);
-                            bookedEntry.setInterval(date);
-                            bookedSlotsCalendar.addEntry(bookedEntry);
-                        }
-                    }
+        if (roomReservationList != null && !roomReservationList.isEmpty()) {
+            for (RoomReservation reservation : roomReservationList) {
+                if (reservation.getRoom().equals(this.room)) {
+                    Entry<RoomReservation> bookedEntry = new Entry<>("Room is booked or unavailable");
+                    LocalTime startTime = convertToLocalTime(reservation.getFromTime());
+                    LocalTime endTime = convertToLocalTime(reservation.getToTime());
+                    LocalDate date = convertToLocalDate(reservation.getFromTime());
+                    bookedEntry.setInterval(startTime, endTime);
+                    bookedEntry.setInterval(date);
+                    bookedSlotsCalendar.addEntry(bookedEntry);
                 }
-
+            }
+        }
+        
         CalendarSource myCalendarSource = new CalendarSource("Calendars");
         myCalendarSource.getCalendars().removeAll();
         myCalendarSource.getCalendars().addAll(bookedSlotsCalendar, myBookingCalendar);
 
-
-        weekView.getCalendarSources().addAll(myCalendarSource);
-        weekView.setRequestedTime(LocalTime.now());
+        roomCal.getCalendarSources().addAll(myCalendarSource);
+        roomCal.setRequestedTime(LocalTime.now());
 
         Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
             @Override
             public void run() {
-                while(true) {
+                while (true) {
                     Platform.runLater(() -> {
-                        weekView.setToday(LocalDate.now());
-                        weekView.setTime(LocalTime.now());
+                        roomCal.setToday(LocalDate.now());
+                        roomCal.setTime(LocalTime.now());
                     });
 
                     try {
@@ -100,7 +92,7 @@ public class RoomCalendarView extends Application {
         updateTimeThread.start();
 
 
-        Scene scene = new Scene(weekView);
+        Scene scene = new Scene(roomCal);
         primaryStage.setTitle("Room Bookings");
         primaryStage.setScene(scene);
         primaryStage.setWidth(1000);
@@ -113,33 +105,12 @@ public class RoomCalendarView extends Application {
         launch();
     }
 
-    private void entryHandler(CalendarEvent e) {
-        Entry<RoomReservation> newEntry = (Entry<RoomReservation>)e.getEntry();
-
-        Date start = convertToDate(newEntry.getStartTime(), newEntry.getStartDate());
-        Date end = convertToDate(newEntry.getEndTime(), newEntry.getStartDate());
-
-        if (e.isEntryAdded()) {
-            //ServerCommunication.addRoomReservation(this.room.getName(), this.room.getBuilding().getId(), userId, start, end);
-        }
-        else if (e.isEntryRemoved()){ System.out.println("entry removed");
-            //ServerCommunication.deleteRoomReservation(e.getEntry().ge);
-        } else {
-            //ServerCommunication.updateRoomReservation(reservationId, old value, new);
-        }
-    }
-
-    public Date convertToDate(LocalTime time, LocalDate date) {
-        LocalDateTime dateTime = LocalDateTime.of(date, time);
-        return Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
-    }
-
-    public LocalTime convertToLocalTime (Date date) {
+    public LocalTime convertToLocalTime(Date date) {
         Instant instant1 = Instant.ofEpochMilli(date.getTime());
         return LocalDateTime.ofInstant(instant1, ZoneId.systemDefault()).toLocalTime();
     }
 
-    public LocalDate convertToLocalDate (Date date) {
+    public LocalDate convertToLocalDate(Date date) {
         Instant instant1 = Instant.ofEpochMilli(date.getTime());
         return LocalDateTime.ofInstant(instant1, ZoneId.systemDefault()).toLocalDate();
     }
