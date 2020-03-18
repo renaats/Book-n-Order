f0bbb862076ea97d5ed9cc031961aebbf74ebed5
mihailsp <m.b.spasov@student.tudelft.nl>
2020-03-18 12:40:44 +0100

    Fixed RegistrationEmailListener class.

--- server/src/main/java/nl/tudelft/oopp/demo/events/RegistrationEmailListener.java
+++ server/src/main/java/nl/tudelft/oopp/demo/events/RegistrationEmailListener.java
@@ -1,37 +1,41 @@
-//package nl.tudelft.oopp.demo.events;
-//
-//import nl.tudelft.oopp.demo.entities.AppUser;
-//import nl.tudelft.oopp.demo.services.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.ApplicationListener;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.UUID;
-//
-//public class RegistrationEmailListener {
-//    @Component
-//    public class RegistrationEmailListener implements ApplicationListener<OnRegistrationSuccessEvent> {
-//        @Autowired
-//        private UserService userService;
-//        @Autowired
-//        private MailSender mailSender;
-//
-//        @Override
-//        public void onApplicationEvent(OnRegistrationSuccessEvent event) {
-//            this.confirmRegistration(event);
-//        }
-//
-//        private void confirmRegistration(OnRegistrationSuccessEvent event) {
-//            AppUser user = event.getUser();
-//            String token = UUID.randomUUID().toString();
-//            userService.createVerificationToken(user,token);
-//            String recipent = user.getEmail();
-//            String subject = "Registration Confirmation";
-//            String url = event.getAppUrl() + "/confirmRegistration?token=" + token;
-//            String message = "Thank you for registering. Please click on the below link to activate your account.";
-//            SimpleMailMessage email = new SimpleMailMessage();
-//
-//         }
-//
-//    }
-//}
+package nl.tudelft.oopp.demo.events;
+
+import nl.tudelft.oopp.demo.entities.AppUser;
+import nl.tudelft.oopp.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+    @Component
+    public class RegistrationEmailListener implements ApplicationListener<OnRegistrationSuccessEvent> {
+        @Autowired
+        private UserService userService;
+        @Autowired
+        private MailSender mailSender;
+
+        @Override
+        public void onApplicationEvent(OnRegistrationSuccessEvent event) {
+            this.confirmRegistration(event);
+        }
+
+        private void confirmRegistration(OnRegistrationSuccessEvent event) {
+            AppUser user = event.getUser();
+            String token = UUID.randomUUID().toString();
+            userService.createVerificationToken(user,token);
+            String recipient = user.getEmail();
+            String subject = "Registration Confirmation";
+            String url = event.getAppUrl() + "/confirmRegistration?token=" + token;
+            String message = "Thank you for registering. Please click on the below link to activate your account.";
+            SimpleMailMessage email = new SimpleMailMessage();
+            email.setTo(recipient);
+            email.setSubject(subject);
+            email.setText(message + "http://localhost:8080" + url);
+            mailSender.send(email);
+         }
+
+    }
+
