ff7319467c2560ddc50b0d5f4a1e4a6583b8ee25
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-18 15:08:14 +0100

    got the change password to work for adminds

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/ChangePasswordController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/ChangePasswordController.java
@@ -15,7 +15,7 @@ import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 public class ChangePasswordController {
 
     @FXML
-    private PasswordField oldPassword;
+    private PasswordField password;
     @FXML
     private PasswordField newPassword2;
     @FXML
@@ -35,6 +35,7 @@ public class ChangePasswordController {
      * @throws IOException should never throw an exception
      */
     public void changePassword() throws IOException {
+        String password = password.getText();
         String password1 = newPassword1.getText();
         String password2 = newPassword2.getText();
         if (password1.equals(password2)) {
