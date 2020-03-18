b9c82b400dd9f8d0100acc4c7cde69abc1f90250
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-18 10:44:04 +0100

    start work on change password

--- client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
@@ -187,6 +187,10 @@ public class ServerCommunication {
         return communicateAndReturnErrorMessage(request);
     }
 
+    public static String ChangePassword(String email, String password) throws UnsupportedEncodingException {
+        HttpRequest request = HttpRequest.newBuilder().GET().header("Authorization", "Bearer " + UserInformation.getBearerKey()).uri(URI.create("http://localhost:8080/user/update?email="+ email + "&password="+URLEncoder.encode(password, "UTF-8"))).POST(HttpRequest.BodyPublishers.noBody()).build();
+        return communicateAndReturnBodyOfResponse(request);
+    }
     /**
      * Removes building hours from the database.
      * @param id = the id of the building.
