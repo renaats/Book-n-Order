7bc908905b55101800671f395b9e8f73b42323bf
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-18 10:44:04 +0100

    start work on change password

--- client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
@@ -616,6 +616,10 @@ public class ServerCommunication {
         return communicateAndReturnErrorMessage(request);
     }
 
+    public static String ChangePassword(String email, String password) throws UnsupportedEncodingException {
+        HttpRequest request = HttpRequest.newBuilder().GET().header("Authorization", "Bearer " + UserInformation.getBearerKey()).uri(URI.create("http://localhost:8080/user/update?email="+ email + "&password="+URLEncoder.encode(password, "UTF-8"))).POST(HttpRequest.BodyPublishers.noBody()).build();
+        return communicateAndReturnBodyOfResponse(request);
+    }
     /**
      * Authorizes the user.
      * @param email User's email
