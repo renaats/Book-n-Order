5cef42737b9185a43e02e310f41e755046beffdd
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-19 17:49:13 +0100

    changes based on gitlab comments

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/ForgotPasswordController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/ForgotPasswordController.java
@@ -8,20 +8,20 @@ import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 public class ForgotPasswordController {
     /**
      * Goes back to the login screen
-     * @throws IOException the input is always the same wo there should not be an exception
+     * @throws IOException the input is always the same so there should not be an exception
      */
     public void loginScene() throws IOException {
         ApplicationDisplay.changeScene("/login-screen.fxml");
     }
 
     /**
-     * should check with the server if the email exist and have the server send an email if it does
+     * Checks with the server if the email exist and have the server send an email if it does
      */
     public void sendEmail() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Password reset");
         alert.setHeaderText(null);
-        alert.setContentText("If the email is correct you will shortly receive an email to recover your account");
+        alert.setContentText("You will shortly receive an email to recover your account");
         alert.showAndWait();
     }
 }
