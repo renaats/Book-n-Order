df34199cc87df16441b06b2289d3cdff4c305ab7
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-21 15:33:53 +0100

    preparing for merge (fixing out of memory issue)

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseEditController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseEditController.java
@@ -1,11 +1,61 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import nl.tudelft.oopp.demo.communication.JsonMapper;
+import nl.tudelft.oopp.demo.communication.ServerCommunication;
+import nl.tudelft.oopp.demo.entities.Bike;
+import nl.tudelft.oopp.demo.entities.Building;
 import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class BikeDatabaseEditController {
+public class BikeDatabaseEditController implements Initializable {
+
+    final ObservableList updateChoiceBoxList = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<Bike> table;
+    @FXML
+    private TableColumn<Bike, String> colId;
+    @FXML
+    private TableColumn<Bike, String> colLocation;
+    @FXML
+    private TableColumn colAvailable;
+    @FXML
+    private TextField buildingDeleteByIdTextField;
+
+    @FXML
+    private ChoiceBox<String> updateChoiceBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colLocation.setCellValueFactory(new PropertyValueFactory<>("Location"));
+        colAvailable.setCellValueFactory(new PropertyValueFactory<>("Available"));
+        loadDataUpdateChoiceBox();
+    }
+
+    /**
+     * Takes care of the options for the updateChoiceBox in the GUI
+     */
+    public void loadDataUpdateChoiceBox() {
+        updateChoiceBoxList.removeAll();
+        String a = "Location";
+        String b = "Available";
+        updateChoiceBoxList.addAll(a, b);
+        updateChoiceBox.getItems().addAll(updateChoiceBoxList);
+    }
 
     /**
      * Changes view to main menu
@@ -38,4 +88,5 @@ public class BikeDatabaseEditController {
     public void goToEditBike() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseEdit.fxml");
     }
+
 }
