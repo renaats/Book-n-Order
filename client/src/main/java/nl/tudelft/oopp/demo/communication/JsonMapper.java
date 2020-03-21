ddd6102b08b4057d6f648830933dc81948e03852
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-21 20:58:56 +0100

    Bike edit search function with Id and with location name work

--- client/src/main/java/nl/tudelft/oopp/demo/communication/JsonMapper.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/JsonMapper.java
@@ -9,10 +9,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javafx.scene.control.Alert;
-import nl.tudelft.oopp.demo.entities.AppUser;
-import nl.tudelft.oopp.demo.entities.Building;
-import nl.tudelft.oopp.demo.entities.Restaurant;
-import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.entities.*;
 
 public class JsonMapper {
 
@@ -182,13 +179,13 @@ public class JsonMapper {
      * @param bikesJson a JSON string representing a list.
      * @return A list filled with object Buildings
      */
-    public static List<Building> bikeListMapper(String bikesJson) {
+    public static List<Bike> bikeListMapper(String bikesJson) {
 
         ObjectMapper mapper = new ObjectMapper();
 
         try {
             // Convert JSON string to Object
-            List<Building> bikes = mapper.readValue(bikesJson, new TypeReference<List<Building>>(){});
+            List<Bike> bikes = mapper.readValue(bikesJson, new TypeReference<List<Bike>>(){});
             return bikes;
         } catch (Exception e) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
