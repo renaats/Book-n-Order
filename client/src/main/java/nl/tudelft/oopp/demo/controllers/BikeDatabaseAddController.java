576201a0c1dae6d5abfa5524fbbb615520355ce9
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-21 17:15:02 +0100

    added add bike functionality

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseAddController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseAddController.java
@@ -5,87 +5,64 @@ import java.io.IOException;
 import java.net.URL;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 
 import nl.tudelft.oopp.demo.communication.JsonMapper;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.entities.Building;
-import nl.tudelft.oopp.demo.errors.CustomAlert;
 import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
-/**
- * Manages the user input from the database add bike view.
- */
+
 public class BikeDatabaseAddController implements Initializable {
 
-    @FXML
-    private ChoiceBox<String> locationsCheckBox;
-    @FXML
-    private ToggleButton availableToggle;
-    @FXML
-    private TextField number;
-
+    public ChoiceBox locationsCheckBox;
+    public Button addButton;
+    public ToggleButton availableToggle;
+    public TextField number;
     private boolean available;
-    private List<Building> buildings;
+    private List<Building> buildings = JsonMapper.buildingListMapper(ServerCommunication.getBuildings());
 
     /**
-     * Will load a list of buildings into the buildings list.
-     */
-    public BikeDatabaseAddController() {
-        try {
-            buildings = JsonMapper.buildingListMapper(ServerCommunication.getBuildings());
-
-        } catch (IOException e) {
-            CustomAlert.warningAlert("There are currently no buildings in the database");
-        }
-    }
-
-    public BikeDatabaseAddController() throws IOException {
-    }
-
-    /**
-     * Loads all the buildings of the database into the choice box as options for bike locations.
+     * loads all the buildings of the database into the choice box as options for bike locations
      */
     private void loadData() {
-        for (Building building : buildings) {
-            locationsCheckBox.getItems().add(building.getName());
+        for (int i = 0; i < buildings.size(); i++) {
+            locationsCheckBox.getItems().add(buildings.get(i).getName());
         }
     }
 
     /**
-     * Changes view to main menu.
-     * @throws IOException should never throw an exception as the input is always the same.
+     * Changes view to main menu
+     * @throws IOException should never throw an exception as the input is always the same
      */
     public void mainMenu() throws IOException {
         ApplicationDisplay.changeScene("/DatabaseMainMenu.fxml");
     }
 
     /**
-     * Changes view to main Bike database menu.
-     * @throws IOException should never throw an exception as the input is always the same.
+     * Changes view to main Bike database menu
+     * @throws IOException should never throw an exception as the input is always the same
      */
     public void goToBikeMenu() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseMenu.fxml");
     }
 
     /**
-     * Changes view to BikeDatabaseAdd.
-     * @throws IOException should never throw an exception as the input is always the same.
+     * Changes view to BikeDatabaseAdd
+     * @throws IOException should never throw an exception as the input is always the same
      */
     public void goToAddBike() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseAdd.fxml");
     }
 
     /**
-     * Changes view to main BikeDatabaseEdit.
-     * @throws IOException should never throw an exception as the input is always the same.
+     * Changes view to main BikeDatabaseEdit
+     * @throws IOException should never throw an exception as the input is always the same
      */
     public void goToEditBike() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseEdit.fxml");
@@ -93,9 +70,10 @@ public class BikeDatabaseAddController implements Initializable {
 
     /**
      * Makes sure the button toggles from false to true every time.
+     * @param e Just an ActionEvent param
      */
     @FXML
-    private void toggleClickAvailable() {
+    private void toggleClickAvailable(ActionEvent e) {
         if (available) {
             availableToggle.setText("False");
             available = false;
@@ -106,42 +84,21 @@ public class BikeDatabaseAddController implements Initializable {
     }
 
     /**
-     * Adds the bike to the database.
+     * Adds the building to the database
      */
     public void databaseAddBike() {
         int buildingId = -1;
-        for (Building building : buildings) {
-            if (building.getName().equals(locationsCheckBox.getValue())) {
-                buildingId = building.getId();
+        for (int i = 0; i < buildings.size(); i++) {
+            if (buildings.get(i).getName().equals(locationsCheckBox.getValue())){
+                buildingId = buildings.get(i).getId();
             }
         }
-        boolean success = false;
-        int numberOfBikes = 0;
-        try {
-            numberOfBikes = Integer.parseInt(number.getText());
-        } catch (NumberFormatException e) {
-            CustomAlert.warningAlert("Please use and integer for the number of bikes");
-            return;
-        }
-        if (numberOfBikes > 0) {
-            String response = "";
-            for (int i = 0; i < Integer.parseInt(number.getText()); i++) {
-                response = ServerCommunication.addBike(buildingId,available);
-                if (response.equals("Successfully added!")) {
-                    success = true;
-                } else {
-                    success = false;
-                    CustomAlert.warningAlert("An error occurred while adding the bikes");
-                    break;
-                }
-            }
-            if (success) {
-                CustomAlert.informationAlert(number.getText() + " Bike/s have been successfully");
-            } else {
-                CustomAlert.warningAlert(response);
-            }
-        } else {
-            CustomAlert.warningAlert("Please select a number bigger than 0");
+        for (int i = 0; i<Integer.parseInt(number.getText()); i++){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Bike adder");
+            alert.setHeaderText(null);
+            alert.setContentText(ServerCommunication.addBike(buildingId,available));
+            alert.showAndWait();
         }
     }
 
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseEditController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseEditController.java
@@ -2,265 +2,214 @@ package nl.tudelft.oopp.demo.controllers;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.ResourceBundle;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import nl.tudelft.oopp.demo.communication.JsonMapper;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.entities.Bike;
 import nl.tudelft.oopp.demo.entities.Building;
-import nl.tudelft.oopp.demo.errors.CustomAlert;
 import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
-/**
- * Manages the user input from the database edit bike view.
- */
 public class BikeDatabaseEditController implements Initializable {
 
-    final ObservableList<Bike> bikeSearchResult = FXCollections.observableArrayList();
-    final ObservableList<Building> locationsSearchResults = FXCollections.observableArrayList();
-
-    @FXML
-    private ChoiceBox<String> locationChangeChoiceBoxSearch;
-    @FXML
-    private ChoiceBox<String> locationChangeChoiceBox;
-    @FXML
-    private CheckBox availableCheckbox;
-    @FXML
-    private TextField bikeDeleteByIdTextField;
-    @FXML
-    private CheckBox available;
     @FXML
     private TableView<Bike> table;
     @FXML
-    private TextField bikeFindByIdTextField;
-    @FXML
-    private TableColumn<Bike, Integer> colId;
-    @FXML
-    private Button confirmDeleteByIdButton;
-    @FXML
-    private CheckBox available;
-    @FXML
-    private ChoiceBox locationChangeChoiceBoxSearch;
-    @FXML
-    private ChoiceBox locationChangeChoiceBox;
-    @FXML
-    private CheckBox availableCheckbox;
-    @FXML
-    private TextField bikeChangeToField;
-    @FXML
-    private TextField bikeDeleteByIdTextField;
-    @FXML
-    private Button confirmDeleteByIdButton;
-    @FXML
-    private CheckBox available;
-    @FXML
-    private TableView table;
-    @FXML
-    private TextField bikeFindByIdTextField;
-    @FXML
-    private TableColumn<Bike, Integer> colId;
+    private TableColumn<Bike, String> colId;
     @FXML
     private TableColumn<Bike, String> colLocation;
     @FXML
-    private TableColumn<Bike, Boolean> colAvailable;
+    private TableColumn colAvailable;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        colLocation.setCellValueFactory(new PropertyValueFactory<>("getBuildingName"));
-        colAvailable.setCellValueFactory(new PropertyValueFactory<>("available"));
+        colLocation.setCellValueFactory(new PropertyValueFactory<>("Location"));
+        colAvailable.setCellValueFactory(new PropertyValueFactory<>("Available"));
+        loadDataUpdateChoiceBox();
+        listBuildingsButtonClicked();
+    }
+
+    /**
+     * Takes care of the options for the updateChoiceBox in the GUI
+     */
+    public void loadDataUpdateChoiceBox() {
+        updateChoiceBoxList.removeAll();
+        String a = "Name";
+        String b = "Street";
+        String c = "House Number";
+        updateChoiceBoxList.addAll(a, b, c);
+        updateChoiceBox.getItems().addAll(updateChoiceBoxList);
+    }
+
+    /**
+     * Handles clicking the list button.
+     */
+    public void listBuildingsButtonClicked() {
         try {
-            loadDataIntoChoiceBox();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        loadBikesIntoTable();
-    }
-
-    /**
-     * Loads the current bikes into the table.
-     */
-    public void loadBikesIntoTable() {
-        List<Bike> bikes = new ArrayList<>(Objects.requireNonNull(JsonMapper.bikeListMapper(ServerCommunication.getBikes())));
-        bikeSearchResult.clear();
-        bikeSearchResult.addAll(bikes);
-        table.setItems(bikeSearchResult);
-    }
-
-    /**
-     * Loads the bikes that have the same value of "available" as the checkBox.
-     */
-    public void loadBikesIntoTableAvailable() {
-        List<Bike> bikes = new ArrayList<>(Objects.requireNonNull(JsonMapper.bikeListMapper(ServerCommunication.getBikes())));
-        bikeSearchResult.clear();
-        for (Bike b : bikes) {
-            if (b.isAvailable() == available.isSelected()) {
-                bikeSearchResult.add(b);
-            }
+            //List<Bike> bikes = new ArrayList<>(Objects.requireNonNull(JsonMapper.bikeListMapper(ServerCommunication.getBuildings())));
+            buildingResult.clear();
+            buildingResult.addAll(bikes);
+            table.setItems(buildingResult);
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("No buildings found.");
+            alert.showAndWait();
         }
     }
-
     /**
-     * Shows the bike with the chosen id in the table.
-     */
-    public void loadBikesIntoTableId() {
-        List<Bike> bikes = new ArrayList<>(Objects.requireNonNull(JsonMapper.bikeListMapper(ServerCommunication.getBikes())));
-        bikeSearchResult.clear();
-        for (Bike bike : bikes) {
-            if (bike.getId() == Integer.parseInt(bikeFindByIdTextField.getText())) {
-                bikeSearchResult.add(bike);
-            }
-        }
-        table.setItems(bikeSearchResult);
-    }
-
-    /**
-     * Loads database bikes into table filtering by location.
-     * @throws IOException throws when there is nothing in the location filter.
-     */
-    public void loadBikesIntoTableLocation() throws IOException {
-        List<Building> locations = new ArrayList<>(Objects.requireNonNull(JsonMapper.buildingListMapper(ServerCommunication.getBuildings())));
-        locationsSearchResults.clear();
-        for (Building location : locations) {
-            if (location.getName().equals(locationChangeChoiceBoxSearch.getValue())) {
-                locationsSearchResults.add(location);
-            }
-        }
-        bikeSearchResult.clear();
-        List<Bike> bikes = new ArrayList<>(Objects.requireNonNull(JsonMapper.bikeListMapper(ServerCommunication.getBikes())));
-        for (Bike bike : bikes) {
-            if (locationsSearchResults.contains(bike.getLocation())) {
-                bikeSearchResult.add(bike);
-            }
-        }
-        table.setItems(bikeSearchResult);
-        loadBikesIntoTable();
-    }
-
-    /**
-     * Takes care of the options for the updateChoiceBox in the GUI.
-     */
-    public void loadDataIntoChoiceBox() throws IOException {
-        List<Building> locations = new ArrayList<>(Objects.requireNonNull(JsonMapper.buildingListMapper(ServerCommunication.getBuildings())));
-        locationsSearchResults.clear();
-        locationsSearchResults.addAll(locations);
-        for (int i = 0; i < locationsSearchResults.size(); i++) {
-            locationChangeChoiceBoxSearch.getItems().add(locations.get(i).getName());
-        }
-        locationsSearchResults.clear();
-    }
-
-    /**
-     * Changes view to main menu.
-     * @throws IOException should never throw an exception as the input is always the same.
+     * Changes view to main menu
+     * @throws IOException should never throw an exception as the input is always the same
      */
     public void mainMenu() throws IOException {
         ApplicationDisplay.changeScene("/DatabaseMainMenu.fxml");
     }
 
     /**
-     * Changes view to main Bike database menu.
-     * @throws IOException should never throw an exception as the input is always the same.
+     * Changes view to main Bike database menu
+     * @throws IOException should never throw an exception as the input is always the same
      */
     public void goToBikeMenu() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseMenu.fxml");
     }
 
     /**
-     * Changes view to BikeDatabaseAdd.
-     * @throws IOException should never throw an exception as the input is always the same.
+     * Changes view to BikeDatabaseAdd
+     * @throws IOException should never throw an exception as the input is always the same
      */
     public void goToAddBike() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseAdd.fxml");
     }
 
     /**
-     * Changes view to main BikeDatabaseEdit.
-     * @throws IOException should never throw an exception as the input is always the same.
+     * Changes view to main BikeDatabaseEdit
+     * @throws IOException should never throw an exception as the input is always the same
      */
     public void goToEditBike() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseEdit.fxml");
     }
 
+
+
+
+
+
+
+
+
+    @FXML
+    private TextField buildingDeleteByIdTextField;
+
+    final ObservableList updateChoiceBoxList = FXCollections.observableArrayList();
+    final ObservableList<Building> buildingResult = FXCollections.observableArrayList();
+
+    @FXML
+    private ChoiceBox<String> updateChoiceBox;
+    @FXML
+    private TextField buildingFindByIdTextField;
+    @FXML
+    private TextField buildingChangeToField;
+
+
     /**
-     * Deletes a bike based on the ID of the text box.
+     * Handles clicking the building find button.
      */
-    public void deleteBikeById() {
-        int id = Integer.parseInt(bikeDeleteByIdTextField.getText());
-        CustomAlert.informationAlert(ServerCommunication.deleteBike(id));
-        loadBikesIntoTable();
+    public void buildingIdButtonClicked() {
+        try {
+            int id = Integer.parseInt(buildingFindByIdTextField.getText());
+            Building building = JsonMapper.buildingMapper(ServerCommunication.findBuilding(id));
+            buildingResult.clear();
+            buildingResult.add(building);
+            table.setItems(buildingResult);
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Missing argument.");
+            alert.showAndWait();
+        }
     }
 
     /**
-     * Changes the attribute of the bike at the top of the table.
+     * Handles clicking of the Remove Building button.
      */
-    public void updateBikeAttribute() {
-        int id = bikeSearchResult.get(0).getId();
-        String ava;
-        if (availableCheckbox.isSelected()) {
-            ava = "true";
-        } else {
-            ava = "false";
+    public void deleteBuildingButtonClicked() {
+        try {
+            int id = Integer.parseInt(buildingDeleteByIdTextField.getText());
+            ServerCommunication.deleteBuilding(id);
+            buildingResult.removeIf(b -> b.getId() == id);
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Missing argument.");
+            alert.showAndWait();
         }
-        if (locationChangeChoiceBox.getValue() != null) {
-            int buildingId = nameGetId(locationChangeChoiceBox.getValue());
-            CustomAlert.informationAlert(ServerCommunication.updateBike(id, "location", "" + buildingId));
-        }
-        ServerCommunication.updateBike(id, "available", ava);
-        loadBikesIntoTable();
     }
 
     /**
-     * Gets the ID out of the string of form "name (ID)".
-     * @param value the name of the given form.
-     * @return the ID present in the initial string.
+     * Handles clicking of the Remove Building button through the table.
      */
-    private int nameGetId(String value) {
-        value = value.replaceAll("\\D+", "");
-        value = value.trim();
-        if (value.equals("")) {
-            return -1;
+    public void deleteBuildingButtonClickedByTable() {
+        try {
+            Building building = table.getSelectionModel().getSelectedItem();
+            ServerCommunication.deleteBuilding(building.getId());
+            buildingResult.removeIf(b -> b.getId().equals(building.getId()));
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Missing argument.");
+            alert.showAndWait();
         }
-        return Integer.parseInt(value);
     }
 
     /**
-     * Deletes the selected bike from the table and the database.
+     * Handles clicking of the edit Building button through the table.
      */
-    public void deleteBikeUsingTable() {
-        Bike bike = table.getSelectionModel().getSelectedItem();
-        CustomAlert.informationAlert(ServerCommunication.deleteBike(bike.getId()));
-        loadBikesIntoTable();
+    public void editBuildingButtonClickedByTable() {
+        try {
+            Building building = table.getSelectionModel().getSelectedItem();
+            buildingFindByIdTextField.setText(Integer.toString(building.getId()));
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Missing argument.");
+            alert.showAndWait();
+        }
     }
 
     /**
-     * Allows you to select the bike you wish to update in the table.
+     * Handles the sending of updated values.
      */
-    public void updateUsingTable() throws IOException {
-        Bike bike = table.getSelectionModel().getSelectedItem();
-        bikeFindByIdTextField.setText("" + bike.getId());
-        loadBikesIntoTableId();
-        availableCheckbox.setSelected(bike.isAvailable());
-        List<Building> locations = new ArrayList<>(Objects.requireNonNull(JsonMapper.buildingListMapper(ServerCommunication.getBuildings())));
-        locationChangeChoiceBox.getItems().clear();
-        for (Building location : locations) {
-            locationChangeChoiceBox.getItems().add(location.getName() + " (" + location.getId() + ")");
+    public void updateBuildingButtonClicked() {
+        try {
+            int id = Integer.parseInt(buildingFindByIdTextField.getText());
+            String attribute = updateChoiceBox.getValue().replaceAll(" ", "");
+            String changeValue = buildingChangeToField.getText();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Building update");
+            alert.setHeaderText(null);
+            alert.setContentText(ServerCommunication.updateBuilding(id, attribute, changeValue));
+            alert.showAndWait();
+            buildingResult.clear();
+            listBuildingsButtonClicked();
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Missing argument.");
+            alert.showAndWait();
         }
     }
+
 }
