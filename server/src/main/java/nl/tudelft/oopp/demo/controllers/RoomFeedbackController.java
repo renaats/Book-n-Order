47e13299490a2a04565a6096247b9b1b004bf47e
Alto Delia <A.delia@student.tudelft.nl>
2020-03-21 01:22:46 +0100

    Added Room Feedback Controller and fixed issues with Bike Feedback Controller

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomFeedbackController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomFeedbackController.java
@@ -1,7 +1,8 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import nl.tudelft.oopp.demo.entities.BikeFeedback;
-import nl.tudelft.oopp.demo.services.BikeFeedbackService;
+import nl.tudelft.oopp.demo.entities.RoomFeedback;
+import nl.tudelft.oopp.demo.services.RoomFeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Repository;
@@ -14,22 +15,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Creates server side endpoints and routes requests to the BikeFeedbackService.
- * Maps all requests that start with "/bike_feedback".
+ * Creates server side endpoints and routes requests to the RoomFeedbackService.
+ * Maps all requests that start with "/room_feedback".
  * Manages access control on a per-method basis.
  */
 @Repository
 @RestController
-@RequestMapping(path = "/bike_feedback")
-public class BikeFeedbackController {
+@RequestMapping(path = "/room_feedback")
+public class RoomFeedbackController {
     @Autowired
-    private BikeFeedbackService bikeFeedbackService;
+    private RoomFeedbackService roomFeedbackService;
 
     /**
-     * Adds a bike feedback.
+     * Adds a room feedback.
      * @param clientEmail = the email of the feedback sender.
      * @param recipientEmail = the email of the feedback recipient.
-     * @param bikeReservationId = the id of the bike reservation being reviewed.
+     * @param roomReservationId = the id of the room reservation being reviewed.
      * @param time = time when the feedback was sent.
      * @param feedback = the feedback text.
      * @return String containing the result of your request.
@@ -37,49 +38,49 @@ public class BikeFeedbackController {
     @Secured("ROLE_USER")
     @PostMapping(path = "/add") // Map ONLY POST Requests
     @ResponseBody
-    public int addNewBikeFeedback(
+    public int addNewRoomFeedback(
             @RequestParam String clientEmail,
             @RequestParam String recipientEmail,
-            @RequestParam int bikeReservationId,
+            @RequestParam int roomReservationId,
             @RequestParam long time,
             @RequestParam String feedback) {
-        return bikeFeedbackService.add(clientEmail, recipientEmail, bikeReservationId, time, feedback);
+        return roomFeedbackService.add(clientEmail, recipientEmail, roomReservationId, time, feedback);
     }
 
     /**
-     * Updates a specified attribute for some bike feedback.
-     * @param id = the id of the bike feedback.
+     * Updates a specified attribute for some room feedback.
+     * @param id = the id of the room feedback.
      * @param attribute = the attribute whose value is changed.
      * @param value = the new value of the attribute.
      * @return String containing the result of your request.
      */
-    @Secured({"ROLE_ADMIN", "ROLE_BIKE"})
+    @Secured({"ROLE_ADMIN", "ROLE_ROOM"})
     @PostMapping(path = "/update")
     @ResponseBody
     public int updateAttribute(@RequestParam int id, @RequestParam String attribute, @RequestParam String value) {
-        return bikeFeedbackService.update(id, attribute, value);
+        return roomFeedbackService.update(id, attribute, value);
     }
 
     /**
-     * Deletes a bike feedback.
-     * @param id = the id of the bike feedback.
+     * Deletes a room feedback.
+     * @param id = the id of the room feedback.
      * @return String containing the result of your request.
      */
-    @Secured({"ROLE_ADMIN", "ROLE_BIKE"})
+    @Secured({"ROLE_ADMIN", "ROLE_ROOM"})
     @DeleteMapping(path = "/delete")
     @ResponseBody
-    public int deleteBikeFeedback(@RequestParam int id) {
-        return bikeFeedbackService.delete(id);
+    public int deleteRoomFeedback(@RequestParam int id) {
+        return roomFeedbackService.delete(id);
     }
 
     /**
-     * Lists all bike feedback.
-     * @return Iterable of all bike reservations.
+     * Lists all room feedback.
+     * @return Iterable of all room reservations.
      */
-    @Secured({"ROLE_ADMIN", "ROLE_BIKE"})
+    @Secured({"ROLE_ADMIN", "ROLE_ROOM"})
     @GetMapping(path = "/all")
     @ResponseBody
-    public Iterable<BikeFeedback> getAllBikeFeedback() {
-        return bikeFeedbackService.all();
+    public Iterable<RoomFeedback> getAllRoomFeedback() {
+        return roomFeedbackService.all();
     }
 }
