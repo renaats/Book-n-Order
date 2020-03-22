10651462140f8ab1884a8a729a501d575c28a2e4
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-22 21:29:21 +0100

    getting the table to show the name and fixing issues caused by the rebase

--- client/src/main/java/nl/tudelft/oopp/demo/communication/JsonMapper.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/JsonMapper.java
@@ -8,16 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javafx.scene.control.Alert;
-import javafx.scene.control.DialogPane;
-import javafx.stage.StageStyle;
-
-import nl.tudelft.oopp.demo.entities.AppUser;
-import nl.tudelft.oopp.demo.entities.Bike;
-import nl.tudelft.oopp.demo.entities.Building;
-import nl.tudelft.oopp.demo.entities.BuildingHours;
-import nl.tudelft.oopp.demo.entities.Restaurant;
-import nl.tudelft.oopp.demo.entities.RestaurantHours;
-import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.entities.*;
 import nl.tudelft.oopp.demo.user.UserInformation;
 
 /**
