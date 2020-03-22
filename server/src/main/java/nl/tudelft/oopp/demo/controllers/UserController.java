648670f1d48638e2a2ce86560ab4a6a59e985d7d
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-22 21:54:24 +0100

    working on problems caused by pull without commit

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/UserController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/UserController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Creates server side endpoints and routes requests to the UserService.
  * Maps all requests that start with "/user".
