cba59c2a3d9c5508cd0e506d5c8db10ef14617ee
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-22 21:54:24 +0100

    working on problems caused by pull without commit

--- server/src/main/java/nl/tudelft/oopp/demo/services/UserService.java
+++ server/src/main/java/nl/tudelft/oopp/demo/services/UserService.java
@@ -10,7 +10,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
+import java.net.URLEncoder;
 import java.util.HashSet;
 import java.util.List;
 
@@ -176,7 +176,7 @@ public class UserService {
      * @param value = the new value of the attribute
      * @return an error code corresponding to the outcome of the request
      */
-    public int update(String email, String attribute, String value) {
+    public int update(String email, String attribute, String value) throws UnsupportedEncodingException {
         if (userRepository.findById(email).isEmpty()) {
             return 419;
         }
@@ -184,7 +184,7 @@ public class UserService {
 
         switch (attribute) {
             case "password":
-                appUser.setPassword(bcryptPasswordEncoder.encode(value));
+                appUser.setPassword(bcryptPasswordEncoder.encode(URLDecoder.decode(value, "UTF-8")));
                 break;
             case "name":
                 appUser.setName(value);
