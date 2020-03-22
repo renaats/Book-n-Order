04435fbcd13f16cd65fe4f0145ca54d38aea3553
Amber Słapczyński <h.k.slapczynski@student.tudelft.nl>
2020-03-22 19:08:28 +0100

    Update DishSpecificationsBuilder.java

--- server/src/main/java/nl/tudelft/oopp/demo/specifications/DishSpecificationsBuilder.java
+++ server/src/main/java/nl/tudelft/oopp/demo/specifications/DishSpecificationsBuilder.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 import nl.tudelft.oopp.demo.entities.Dish;
 import org.springframework.data.jpa.domain.Specification;
 
+/**
+ * Used for creating a compound specification based on multiple parameters.
+ */
 public class DishSpecificationsBuilder {
 
     private final List<SearchCriteria> params;
