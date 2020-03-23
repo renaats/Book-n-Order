ae1ed8860333d0236a21f54627c1c42279f58a8f
Alto Delia <A.delia@student.tudelft.nl>
2020-03-23 05:41:16 +0100

    Added Controllers to Feedback Scenes

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/LeaveBikeFeedbackController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/LeaveBikeFeedbackController.java
@@ -1,4 +1,25 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
 public class LeaveBikeFeedbackController {
+
+    @FXML
+    public TextField bikeFeedbackText;
+
+    public final int maxFieldLength = 150;
+
+    public void submitBikeFeedback() {
+        if (bikeFeedbackText.getText() == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Authenticator");
+            alert.setHeaderText(null);
+            alert.setContentText("Please type something.");
+            alert.showAndWait();
+        } else {
+            //ServerCommunication.addBikeFeedback(client, recipient, reservationId, current time, bikeFeedbackText.text());
+        }
+    }
 }
