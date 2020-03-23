e27c4b2d6a01b32dc41f2f83147dcb4c9533e678
Alto Delia <A.delia@student.tudelft.nl>
2020-03-23 05:41:16 +0100

    Added Controllers to Feedback Scenes

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/LeaveRoomFeedbackController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/LeaveRoomFeedbackController.java
@@ -1,4 +1,26 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+
 public class LeaveRoomFeedbackController {
+
+    @FXML
+    public TextField roomFeedbackText;
+
+    public final int maxFieldLength = 150;
+
+    public void submitRoomFeedback() {
+        if (roomFeedbackText.getText() == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Authenticator");
+            alert.setHeaderText(null);
+            alert.setContentText("Please type something.");
+            alert.showAndWait();
+        } else {
+            //ServerCommunication.addRoomFeedback(client, recipient, reservationId, current time, roomFeedbackText.text());
+        }
+    }
 }
