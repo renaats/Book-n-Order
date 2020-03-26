48e68da2886ce2a76262f5eb9981163859fab006
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-26 13:54:05 +0100

    Put the code up to standard

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseMenuController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseMenuController.java
@@ -1,7 +1,9 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import java.io.IOException;
+
 import java.net.URL;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -9,17 +11,25 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import javafx.stage.StageStyle;
 import nl.tudelft.oopp.demo.communication.JsonMapper;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.entities.Bike;
 import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
-
+/**
+ * Manages the user input from the database bike menu view
+ */
 public class BikeDatabaseMenuController implements Initializable {
 
     final ObservableList<Bike> bikeSearchResult = FXCollections.observableArrayList();
@@ -50,6 +60,15 @@ public class BikeDatabaseMenuController implements Initializable {
             bikeSearchResult.clear();
             bikeSearchResult.addAll(bikes);
             table.setItems(bikeSearchResult);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle(null);
+            alert.setHeaderText(null);
+            alert.setContentText("There are currently no bikes in the database");
+            alert.initStyle(StageStyle.UNDECORATED);
+            DialogPane dialogPane = alert.getDialogPane();
+            dialogPane.getStylesheets().add(getClass().getResource("/alertWarning.css").toExternalForm());
+            alert.showAndWait();
         }
     }
 
