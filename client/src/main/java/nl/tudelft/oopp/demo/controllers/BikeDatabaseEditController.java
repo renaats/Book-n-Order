dc79b39f044a5def2777a0d2d43d4e29f7ad4f99
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-26 14:00:48 +0100

    Edited imports to fix the checkstyle

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseEditController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseEditController.java
@@ -13,8 +13,16 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.StageStyle;
 
 import javafx.stage.StageStyle;
 
