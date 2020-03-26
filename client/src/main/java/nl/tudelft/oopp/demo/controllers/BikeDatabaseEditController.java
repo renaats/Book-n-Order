a7c6e646999121bf0975f062cf09d336c28013df
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-26 15:16:08 +0100

    import seperators

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseEditController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseEditController.java
@@ -22,6 +22,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+
 import javafx.stage.StageStyle;
 
 import javafx.stage.StageStyle;
