3d33ac1221962488828a122cd2a39e5762629edb
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-27 10:18:38 +0100

    added full stops

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseMenuController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/BikeDatabaseMenuController.java
@@ -36,7 +36,7 @@ import nl.tudelft.oopp.demo.entities.Bike;
 import nl.tudelft.oopp.demo.views.ApplicationDisplay;
 
 /**
- * Manages the user input from the database bike menu view
+ * Manages the user input from the database bike menu view.
  */
 public class BikeDatabaseMenuController implements Initializable {
 
@@ -60,7 +60,7 @@ public class BikeDatabaseMenuController implements Initializable {
     }
 
     /**
-     * Loads all bike of the database into the table
+     * Loads all bike of the database into the table.
      */
     private void loadBikesIntoTable() {
         if (JsonMapper.bikeListMapper(ServerCommunication.getBikes()) != null) {
@@ -81,24 +81,24 @@ public class BikeDatabaseMenuController implements Initializable {
     }
 
     /**
-     * Changes view to the database main menu when the home icon is clicked
-     * @throws IOException should never throw an IOException as the input is always the same
+     * Changes view to the database main menu when the home icon is clicked.
+     * @throws IOException should never throw an IOException as the input is always the same.
      */
     public void mainMenu() throws IOException {
         ApplicationDisplay.changeScene("/DatabaseMainMenu.fxml");
     }
 
     /**
-     * Changes view to the database bike add when the add bikes button is clicked
-     * @throws IOException should never throw an IOException as the input is always the same
+     * Changes view to the database bike add when the add bikes button is clicked.
+     * @throws IOException should never throw an IOException as the input is always the same.
      */
     public void goToAddBikes() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseAdd.fxml");
     }
 
     /**
-     * Changes view to the database bike edit when the add bikes button is clicked
-     * @throws IOException should never throw an IOException as the input is always the same
+     * Changes view to the database bike edit when the add bikes button is clicked.
+     * @throws IOException should never throw an IOException as the input is always the same.
      */
     public void goToEditBikes() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseEdit.fxml");
