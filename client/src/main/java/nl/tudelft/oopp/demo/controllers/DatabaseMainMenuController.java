66b61c9f40cf71748a31e3a6321d87354773ae33
alex <a.delossantossubirats@student.tudelft.nl>
2020-03-27 18:15:47 +0100

    Fixed spelling mistakes and changed the add bikes method
    The addbikes method now check the server response for every bike, and not just the first one, as well as added a check to the bike add controller to make sure the number of bikes is an integer

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/DatabaseMainMenuController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/DatabaseMainMenuController.java
@@ -22,7 +22,7 @@ public class DatabaseMainMenuController {
         ApplicationDisplay.changeScene("/mainMenu.fxml");
     }
 
-    public void goToBikeDatabse() throws IOException {
+    public void goToBikeDatabase() throws IOException {
         ApplicationDisplay.changeScene("/BikeDatabaseMenu.fxml");
     }
 }
