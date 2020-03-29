45e68ffa5ab6341b9115bcfc5dce7ed228b24d6a
Alto Delia <A.delia@student.tudelft.nl>
2020-03-29 04:33:46 +0200

    Fixed checkstyle issue regarding presence of the general ' .* ' import form.

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/MenuController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/MenuController.java
@@ -1,12 +1,19 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import nl.tudelft.oopp.demo.entities.Menu;
-import nl.tudelft.oopp.demo.entities.Restaurant;
 import nl.tudelft.oopp.demo.services.MenuService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Creates server side endpoints and routes requests to the MenuService.
