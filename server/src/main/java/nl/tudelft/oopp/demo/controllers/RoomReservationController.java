598d3e07981d86f597b3f3aa046cbe1a78b6b356
Alto Delia <A.delia@student.tudelft.nl>
2020-03-31 04:35:17 +0200

    fixed presence of '.*' causing checkstyle issues.

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomReservationController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomReservationController.java
@@ -13,7 +13,14 @@ import nl.tudelft.oopp.demo.services.RoomReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Creates server side endpoints and routes requests to the RoomReservationService.
