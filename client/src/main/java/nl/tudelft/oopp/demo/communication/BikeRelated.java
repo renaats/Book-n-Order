a3edbfc9066f4a5c4eae069d4c177b49c1a65ef3
mihailsp <m.b.spasov@student.tudelft.nl>
2020-04-02 11:31:34 +0200

    Split the methods and created the appropriate classes that handle specific server communication.

---
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/BikeRelated.java
@@ -0,0 +1,62 @@
+package nl.tudelft.oopp.demo.communication;
+
+import nl.tudelft.oopp.demo.authentication.AuthenticationKey;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+
+import static nl.tudelft.oopp.demo.communication.ServerCommunication.communicateAndReturnBodyOfResponse;
+import static nl.tudelft.oopp.demo.communication.ServerCommunication.communicateAndReturnErrorMessage;
+
+/**
+ * Controls all client to server communication related to the bike entity
+ * Sends the appropriate HTTP request depending on the method
+ */
+public class BikeRelated {
+
+    /**
+     * gets all bike reservations from the database.
+     * @return the body of the response from the server.
+     */
+    public static String getAllBikeReservations() {
+        HttpRequest request = HttpRequest.newBuilder().GET().header("Authorization", "Bearer " + AuthenticationKey.getBearerKey()).uri(URI.create("http://localhost:8080/bike_reservation/all")).build();
+        return communicateAndReturnBodyOfResponse(request);
+    }
+
+    /**
+     * Finds all past bike reservations for the user that sends the request.
+     * @return the body of the response from the server.
+     */
+    public static String getAllPreviousBikeReservations() {
+        HttpRequest request = HttpRequest.newBuilder().GET().header("Authorization", "Bearer " + AuthenticationKey.getBearerKey()).uri(URI.create("http://localhost:8080/bike_reservation/past")).build();
+        return communicateAndReturnBodyOfResponse(request);
+    }
+
+    /**
+     * Finds all future and current bike reservations for the user that sends the request.
+     * @return the body of the response from the server.
+     */
+    public static String getAllFutureBikeReservations() {
+        HttpRequest request = HttpRequest.newBuilder().GET().header("Authorization", "Bearer " + AuthenticationKey.getBearerKey()).uri(URI.create("http://localhost:8080/bike_reservation/future")).build();
+        return communicateAndReturnBodyOfResponse(request);
+    }
+
+    /**
+     * Finds all active bike reservations for the user that sends the request.
+     * @return the body of the response from the server.
+     */
+    public static String getAllActiveBikeReservations() {
+        HttpRequest request = HttpRequest.newBuilder().GET().header("Authorization", "Bearer " + AuthenticationKey.getBearerKey()).uri(URI.create("http://localhost:8080/bike_reservation/active")).build();
+        return communicateAndReturnBodyOfResponse(request);
+    }
+
+    /**
+     * Cancels a bike reservation for the user that sends the request.
+     * @param id the id of the bike reservation.
+     * @return the error message corresponding to the server's response.
+     */
+    public static String cancelBikeReservation(int id) {
+        HttpRequest request = HttpRequest.newBuilder().GET().header("Authorization", "Bearer " + AuthenticationKey.getBearerKey()).uri(URI.create("http://localhost:8080/bike_reservation/cancel/" + id)).build();
+        return communicateAndReturnErrorMessage(request);
+    }
+}
