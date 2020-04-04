9231c2c13ecb7732faf2a6832fcedb795f2263cf
alex <a.delossantossubirats@student.tudelft.nl>
2020-04-04 22:58:37 +0200

    Cleaned up imports

--- client/src/main/java/nl/tudelft/oopp/demo/entities/FoodOrder.java
+++ client/src/main/java/nl/tudelft/oopp/demo/entities/FoodOrder.java
@@ -6,10 +6,6 @@ import java.util.Date;
 import java.util.Objects;
 import java.util.Set;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.LongProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
