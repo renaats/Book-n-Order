fcae621465ffe43f848f5ebb9a17c34a43ca7ccb
ljongejans <l.j.jongejans@student.tudelft.nl>
2020-04-04 21:30:37 +0200

    Added a campus map in a view and a controller for this view to go back to main menu.

---
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/CampusMapController.java
@@ -0,0 +1,20 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import java.io.IOException;
+
+import nl.tudelft.oopp.demo.views.ApplicationDisplay;
+
+/**
+ * Loads the correct content into the FXML objects that need to display server information and
+ * controls all the user inputs made through the GUI in the "CampusMap.fxml" file
+ */
+public class CampusMapController {
+
+    /**
+     * method changes the view to that of the main menu
+     * @throws IOException the method will never throw an exception
+     */
+    public void mainMenu() throws IOException {
+        ApplicationDisplay.changeScene("/MainMenu.fxml");
+    }
+}
