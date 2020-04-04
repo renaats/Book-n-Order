a7fc37e56af2435073859981e97f3cda0fbe2f27
alex <a.delossantossubirats@student.tudelft.nl>
2020-04-04 21:14:49 +0200

    Added missing import and added int feedback counter to databaseCode.txt

--- server/src/test/java/nl/tudelft/oopp/demo/entities/FoodOrderTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/entities/FoodOrderTest.java
@@ -1,6 +1,7 @@
 package nl.tudelft.oopp.demo.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
