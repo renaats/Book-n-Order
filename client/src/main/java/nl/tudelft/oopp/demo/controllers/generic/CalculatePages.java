b3018e0f0334abd3c67c765fe6c97d8320621999
rjursevskis <r.jursevskis@student.tudelft.nl>
2020-04-06 23:26:16 +0300

    Reduced code length in OrderFoodChooseRestaurantController

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/generic/CalculatePages.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/generic/CalculatePages.java
@@ -9,6 +9,17 @@ import javafx.util.Pair;
  * Takes care of calculating and displaying the right pages for some table.
  */
 public class CalculatePages {
+
+    /**
+     * Calculates the pages for some table.
+     * @param itemList = the list of items.
+     * @param total = the total amount of pages.
+     * @param page = the current page.
+     * @param items = the list of items.
+     * @param size = the size of one page.
+     * @param <T> = some type.
+     * @return a pair of the new page and new total size.
+     */
     public static <T> Pair<Integer, Integer> calculatePages(ObservableList<T> itemList, Integer total, Integer page, List<T> items, Integer size) {
         itemList.clear();
         total = (int) (Math.ceil(items.size() / ((double) size)));
@@ -17,6 +28,9 @@ public class CalculatePages {
         }
         if (page == 0 && items.size() != 0) {
             page = 1;
+        }
+        if (total > 0) {
+            page = Math.max(page, 1);
         }
         if (items.size() > size) {
             for (int i = 0; i < size; i++) {
